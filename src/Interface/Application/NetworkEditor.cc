ad6883047567b05d0a13f15939085ad6acfc4fa4
Dan White <dwhite@sci.utah.edu>
2012-04-30 14:47:02 -0600

    Add check for full input port in GUI.  This will also need to be moved down to domain layer.

--- src/Interface/Application/NetworkEditor.cc
+++ src/Interface/Application/NetworkEditor.cc
@@ -92,6 +92,7 @@ void NetworkEditor::setNetworkEditorController(boost::shared_ptr<NetworkEditorCo
     connect(controller_.get(), SIGNAL(moduleAdded(const std::string&, const SCIRun::Domain::Networks::ModuleInfoProvider&)), 
       this, SLOT(addModule(const std::string&, const SCIRun::Domain::Networks::ModuleInfoProvider&)));
 
+    //TODO: flip this
     connect(this, SIGNAL(addConnection(const SCIRun::Domain::Networks::ConnectionDescription&)), 
       controller_.get(), SLOT(addConnection(const SCIRun::Domain::Networks::ConnectionDescription&)));
 
--- src/Interface/Application/Port.cc
+++ src/Interface/Application/Port.cc
@@ -198,7 +198,7 @@ bool PortWidget::tryConnectPort(const QPointF& pos, PortWidget* port)
     }
     else
     {
-      std::cout << "ports are different datatype or same i/o type, should not be connected" << std::endl;
+      std::cout << "input port is full, or ports are different datatype or same i/o type: should not be connected.  this message should come from the domain layer!" << std::endl;
     }
   }
   return false;
@@ -208,6 +208,8 @@ bool PortWidget::tryConnectPort(const QPointF& pos, PortWidget* port)
 bool PortWidget::canBeConnected(PortWidget* other) const
 {
   if (!other)
+    return false;
+  if (other->isInput() && !other->connections_.empty())
     return false;
   return color() == other->color() &&
     isInput() != other->isInput();
