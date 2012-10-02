bf03169858633eb8125b4d9f9a7ec02d4531041b
Dan White <dwhite@sci.utah.edu>
2012-10-02 16:10:47 -0600

    Fix regex.

--- src/Core/Utils/Exception.cc
+++ src/Core/Utils/Exception.cc
@@ -51,7 +51,7 @@ std::string ExceptionBase::typeName() const
   //TODO very hacky.
   std::string name = typeid(*this).name();
 
-  static boost::regex r(".*\\<(.+)\\>");
+  static boost::regex r(".*class.*<.*struct (.+)>");
   boost::smatch match;
   if (boost::regex_match(name, match, r))
     return (std::string)match[1];
