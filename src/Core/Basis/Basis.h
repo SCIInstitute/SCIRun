5306eafa00c84e4473b8fe05bb003024ddece3c5
Dan White <dwhite@sci.utah.edu>
2012-10-31 16:42:12 -0600

    Fix duplicate class.

--- src/Core/Basis/Basis.h
+++ src/Core/Basis/Basis.h
@@ -43,11 +43,6 @@ namespace SCIRun {
 namespace Core {
 namespace Basis {
 
-  template <class T, size_t N>
-  class StackVector : public boost::array<T,N>
-  {
-  };
-
 //! Class for describing interfaces to basis elements
 template <class T>
 class BasisSimple 
