cbb54e0af30544e6bdc4e67b80ac4cabd0dbec6a
Dan White <dwhite@sci.utah.edu>
2013-04-14 16:22:12 -0600

    Prepare CMake

--- src/Core/Algorithms/Legacy/Fields/share.h
+++ src/Core/Algorithms/Legacy/Fields/share.h
@@ -29,16 +29,11 @@
 #undef SCISHARE
 
 #if defined(_WIN32) && !defined(BUILD_SCIRUN_STATIC)
-#  ifdef BUILD_Core_Algorithms_Fields
+#  ifdef BUILD_Core_Algorithms_Legacy_Fields
 #    define SCISHARE __declspec(dllexport)
 #  else
 #    define SCISHARE __declspec(dllimport)
 #  endif
 #else
 #  define SCISHARE
-#endif
-
-#if defined(min)
-#undef min
-#undef max
 #endif
