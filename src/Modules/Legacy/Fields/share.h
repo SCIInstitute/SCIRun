db0b5258639f5b60953254107f03936c0e8236ca
Dan White <dwhite@sci.utah.edu>
2013-04-14 16:50:19 -0600

    Prepare CMake

--- src/Modules/Legacy/Fields/share.h
+++ src/Modules/Legacy/Fields/share.h
@@ -30,20 +30,11 @@
 #undef SCISHARE
 
 #if defined(_WIN32) && !defined(BUILD_SCIRUN_STATIC)
-#  ifdef BUILD_Dataflow_Modules_Fields
+#  ifdef BUILD_Dataflow_Modules_Legacy_Fields
 #    define SCISHARE __declspec(dllexport)
 #  else
 #    define SCISHARE __declspec(dllimport)
 #  endif
 #else
 #  define SCISHARE
-#endif
-
-// FYI: This should fix the problem that Windows has a min() and max() macro
-// As this should be the last header file that is included, this should take 
-// care of the problem.
-
-#if defined(min)
-#undef min
-#undef max
 #endif
