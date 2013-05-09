0d50f6663bbc7f366c9db6a9aeef5feb0f4cb5f2
Dan White <dwhite@sci.utah.edu>
2013-05-09 15:49:02 -0600

    Update script to print all field types to a file

--- src/ExampleNets/readAllFields.py
+++ src/ExampleNets/readAllFields.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import time
 import SCIRunPythonAPI; from SCIRunPythonAPI import *
 
 def allFields(path):
@@ -10,11 +11,28 @@ def allFields(path):
 				names.append(os.path.join(dirname, filename))
 	return names
 	
+def printList(list, name):
+	thefile = open(name, 'w')
+	for f,v in list:
+		thefile.write("%s\n\t%s\n" % (f,v))
+	
+
 dir = r"E:\scirun\trunk_ref\SCIRunData"
+
+values = []
+files = []
 
 for file in allFields(dir):
 	read = addModule("ReadField")
 	read.Filename = file
+	files.append(file)
 	show = addModule("ReportFieldInfo")
+	prnt = addModule("PrintDatatype")
 	read.output[0] >> show.input.Input
-#executeAll()
+	show.output[0] >> prnt.input[0]
+	executeAll()
+	time.sleep(1)
+	values.append(prnt.ReceivedValue)
+	[removeModule(m.id) for m in modules()]
+
+printList(zip(files, values), r'E:\fieldTypes.txt')
