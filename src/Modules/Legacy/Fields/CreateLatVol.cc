e5aac4e4d3b75bfe4be912dcff6cc9cebc941631
Dan White <dwhite@sci.utah.edu>
2013-08-30 13:48:21 -0600

    Set defaults for CreateLatVol

--- src/Modules/Legacy/Fields/CreateLatVol.cc
+++ src/Modules/Legacy/Fields/CreateLatVol.cc
@@ -63,15 +63,19 @@ AlgorithmParameterName CreateLatVol::ElementSizeNormalized("ElementSizeNormalize
 
 CreateLatVol::CreateLatVol()
   : Module(ModuleLookupInfo("CreateLatVol", "NewField", "SCIRun"))
-#ifdef SCIRUN4_CODE_TO_BE_ENABLED_LATER
-  size_x_(get_ctx()->subVar("sizex"), 16),
-  size_y_(get_ctx()->subVar("sizey"), 16),
-  size_z_(get_ctx()->subVar("sizez"), 16),
-  padpercent_(get_ctx()->subVar("padpercent"), 0.0),
-  data_at_(get_ctx()->subVar("data-at"), "Nodes"),
-  element_size_(get_ctx()->subVar("element-size"),"Mesh")
-#endif
 {
+  setDefaults();
+}
+
+void CreateLatVol::setDefaults()
+{
+  auto state = get_state();
+  state->setValue(XSize, 16);
+  state->setValue(YSize, 16);
+  state->setValue(ZSize, 16);
+  state->setValue(PadPercent, 0.0);
+  state->setValue(DataAtLocation, std::string("Nodes"));
+  state->setValue(ElementSizeNormalized, true);
 }
 
 
