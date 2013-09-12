d66634cc4b8fad6d4f45035ce7fb89aca6f6008a
Dan White <dwhite@sci.utah.edu>
2013-09-12 10:06:17 -0600

    Next up: CQS2TS

---
+++ src/Core/Algorithms/Field/Tests/ConvertMeshToTriSurfMeshAlgoTests.cc
@@ -0,0 +1,90 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2012 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   License for the specific language governing rights and limitations under
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+ 
+#include <gtest/gtest.h>
+
+#include <Core/Datatypes/Legacy/Field/VField.h>
+#include <Core/Datatypes/Legacy/Field/FieldInformation.h>
+#include <Core/Datatypes/Matrix.h>
+#include <Core/Algorithms/Base/AlgorithmPreconditions.h>
+#include <Core/Algorithms/Legacy/Fields/MeshDerivatives/GetFieldBoundaryAlgo.h>
+#include <Testing/Utils/SCIRunUnitTests.h>
+
+using namespace SCIRun;
+using namespace SCIRun::Core::Datatypes;
+using namespace SCIRun::Core::Geometry;
+using namespace SCIRun::Core::Algorithms::Fields;
+using namespace SCIRun::TestUtils;
+
+void runTest(...)
+{
+  //std::cout << "Basis # " << basis << std::endl;
+  //FieldInformation lfi("LatVolMesh", basis, "double");
+
+  //size_type sizex = 2, sizey = 3, sizez = 4;
+  //Point minb(-1.0, -1.0, -1.0);
+  //Point maxb(1.0, 1.0, 1.0);
+  //MeshHandle mesh = CreateMesh(lfi,sizex, sizey, sizez, minb, maxb);
+  //FieldHandle ofh = CreateField(lfi,mesh);
+  //ofh->vfield()->clear_all_values();
+
+  //GetFieldBoundaryAlgo algo;
+
+  //FieldHandle boundary;
+  //MatrixHandle mapping;
+  //algo.run(ofh, boundary, mapping);
+
+  //ASSERT_TRUE(boundary);
+
+  ////TODO: need assertions on boundary field
+
+  //if (basis != -1)
+  //{
+  //  ASSERT_TRUE(mapping);
+  //  EXPECT_EQ(expectedMatrixRows, mapping->nrows());
+  //  EXPECT_EQ(expectedMatrixColumns, mapping->ncols());
+  //  std::ostringstream ostr;
+  //  ostr << *mapping;
+  //  //std::cout << "expected\n" << expectedMatrixString << std::endl;
+  //  //std::cout << "actual\n" << ostr.str() << std::endl;
+  //  EXPECT_EQ(expectedMatrixString, ostr.str());
+  //}
+}
+
+TEST(ConvertMeshToTriSurfAlgoTests, Foo)
+{
+  FAIL() << "TODO";
+  /*
+  EXPECT_EQ("GenericField<LatVolMesh<HexTrilinearLgn<Point> > ,NoDataBasis<double> ,FData3d<double,LatVolMesh<HexTrilinearLgn<Point> > > > ", info.type);
+  EXPECT_EQ(0, info.dataMin);
+  EXPECT_EQ(0, info.dataMax);
+  EXPECT_EQ(0, info.numdata_);
+  EXPECT_EQ(sizex * sizey * sizez, info.numnodes_);
+  EXPECT_EQ((sizex-1) * (sizey-1) * (sizez-1), info.numelements_);
+  EXPECT_EQ("None (nodata basis)", info.dataLocation);*/
+}
