9a5e4f5bd48b7bee8a608fcf0da48b35088d60d9
Dan White <dwhite@sci.utah.edu>
2014-04-11 10:46:46 -0600

    Basic parser tests with a field

--- src/Core/Parser/Tests/ParserTests.cc
+++ src/Core/Parser/Tests/ParserTests.cc
@@ -27,3 +27,74 @@
 */
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <Core/Datatypes/Legacy/Field/Field.h>
+#include <Core/Datatypes/Legacy/Field/VField.h>
+#include <Core/Datatypes/Legacy/Field/FieldInformation.h>
+#include <Core/Parser/ArrayMathEngine.h>
+
+using namespace SCIRun;
+using namespace SCIRun::Core::Geometry;
+using ::testing::NotNull;
+
+namespace
+{
+  FieldHandle CreateEmptyLatVol(size_type sizex = 3, size_type sizey = 4, size_type sizez = 5)
+  {
+    FieldInformation lfi("LatVolMesh", 1, "double");
+    Point minb(-1.0, -1.0, -1.0);
+    Point maxb(1.0, 1.0, 1.0);
+    MeshHandle mesh = CreateMesh(lfi,sizex, sizey, sizez, minb, maxb);
+    FieldHandle ofh = CreateField(lfi,mesh);
+    ofh->vfield()->clear_all_values();
+    return ofh;
+  }
+}
+
+TEST(BasicParserTests, CanCreateEngine)
+{
+  NewArrayMathEngine engine;
+  EXPECT_FALSE(engine.run());
+}
+
+TEST(BasicParserTests, CreateFieldDataDuplicate)
+{
+  FieldHandle field(CreateEmptyLatVol());
+  auto ivfield = field->vfield();
+  double min, max;
+  ivfield->minmax(min,max);
+  EXPECT_EQ(0, min);
+  EXPECT_EQ(0, max);
+  
+  NewArrayMathEngine engine;
+  
+  std::string format = "double";
+  std::string basis = "Linear";
+  int basis_order = 1;
+  
+  ASSERT_TRUE(engine.add_input_fielddata_location("POS",field,basis_order));
+  
+  ASSERT_TRUE(engine.add_input_fielddata_coordinates("X","Y","Z",field,basis_order));
+  
+  ASSERT_TRUE(engine.add_input_fielddata_element("ELEMENT",field,basis_order));
+  
+  ASSERT_TRUE(engine.add_output_fielddata("RESULT",field,basis_order,format));
+  
+  ASSERT_TRUE(engine.add_index("INDEX"));
+  ASSERT_TRUE(engine.add_size("SIZE"));
+  
+  std::string function = "RESULT = X + Y + Z;";
+  ASSERT_TRUE(engine.add_expressions(function));
+  
+  ASSERT_TRUE(engine.run());
+ 
+  FieldHandle ofield;
+  engine.get_field("RESULT",ofield);
+
+  ASSERT_THAT(ofield, NotNull());
+  auto ovfield = ofield->vfield();
+  ovfield->minmax(min,max);
+  EXPECT_EQ(-3, min);
+  EXPECT_EQ(3, max);
+}
