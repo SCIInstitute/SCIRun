48ac39d3eb8898fa04afa7216b667eb98bec23c5
Dan White <dwhite@sci.utah.edu>
2014-04-16 14:44:19 -0600

    CreateFieldData works

---
+++ src/Interface/Modules/Fields/CreateFieldDataDialog.cc
@@ -0,0 +1,70 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2012 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   License for the specific language governing rights and limitations under
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Interface/Modules/Fields/CreateFieldDataDialog.h>
+#include <Modules/Legacy/Fields/CreateFieldData.h>
+#include <Dataflow/Network/ModuleStateInterface.h>  //TODO: extract into intermediate
+
+using namespace SCIRun::Gui;
+using namespace SCIRun::Dataflow::Networks;
+typedef SCIRun::Modules::Fields::CreateFieldData CreateFieldDataModule;
+
+CreateFieldDataDialog::CreateFieldDataDialog(const std::string& name, ModuleStateHandle state,
+  QWidget* parent /* = 0 */)
+  : ModuleDialogGeneric(state, parent)
+{
+  setupUi(this);
+  setWindowTitle(QString::fromStdString(name));
+  fixSize();
+  
+  connect(functionTextEdit_, SIGNAL(textChanged()), this, SLOT(push()));
+  connect(fieldOutputDataComboBox_, SIGNAL(activated(const QString&)), this, SLOT(push()));
+  connect(fieldOutputBasisComboBox_, SIGNAL(activated(const QString&)), this, SLOT(push()));
+}
+
+void CreateFieldDataDialog::push()
+{
+  if (!pulling_)
+  {
+    state_->setValue(CreateFieldDataModule::FunctionString, functionTextEdit_->toPlainText().toStdString());
+#ifdef SCIRUN4_CODE_TO_BE_ENABLED_LATER
+    state_->setValue(CreateFieldDataModule::FormatString,
+    state_->setValue(CreateFieldDataModule::BasisString,
+#endif
+  }
+}
+
+void CreateFieldDataDialog::pull()
+{
+  Pulling p(this);
+  auto newValue = QString::fromStdString(state_->getValue(CreateFieldDataModule::FunctionString).getString());
+  if (newValue != functionTextEdit_->toPlainText())
+    functionTextEdit_->setPlainText(newValue);
+  
+  //hook up format, basis later.
+}
