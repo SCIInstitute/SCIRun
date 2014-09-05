adcf50b746e98b3bc4e5eeadbad77e3a09c9fca8
Moritz <moritz@sci.utah.edu>
2014-09-05 12:49:16 -0600

    intermediate commit

--- src/Core/Algorithms/Math/AddKnownsToLinearSystem.cc
+++ src/Core/Algorithms/Math/AddKnownsToLinearSystem.cc
@@ -63,7 +63,7 @@ bool AddKnownsToLinearSystemAlgo::run(SparseRowMatrixHandle stiff,
   SparseRowMatrixFromMap::Values additionalData;
 	
 	// Making sure the stiff matrix (left hand side) is symmetric
-  if (!isSymmetricMatrix(*stiff))
+  if (!(*stiff).isSymmetric())
     THROW_ALGORITHM_INPUT_ERROR("matrix A is not symmetrical");
     
 	// Storing the number of columns in m and rows in n from the stiff matrix, m == n
@@ -71,14 +71,13 @@ bool AddKnownsToLinearSystemAlgo::run(SparseRowMatrixHandle stiff,
                n = static_cast<unsigned int>(stiff->nrows());
 	
 	// Checking if the rhs matrix is allocated and that the dimenions agree with the stiff matrix
-	if (!rhs)
-	{
-    THROW_ALGORITHM_INPUT_ERROR("Could not allocate new b matrix");
-	}
-	else if ( !(((rhs->ncols() == m) && (rhs->nrows() == 1)) || ((rhs->ncols() == 1) && (rhs->nrows() == m))) )
-	{
+	if (rhs)
+        {
+	 if ( !(((rhs->ncols() == m) && (rhs->nrows() == 1)) || ((rhs->ncols() == 1) && (rhs->nrows() == m))) )
+	 {
 		THROW_ALGORITHM_INPUT_ERROR("The dimensions of vector b do not match the dimensions of matrix A"); 
-  }
+         }
+        }
     
 	// casting rhs to be a column
 	auto rhsCol = matrix_cast::as_column(rhs);
