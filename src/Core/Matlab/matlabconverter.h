0cc68f81e09c52d535b727e38e60b4d97fb33bca
Dan White <dwhite@sci.utah.edu>
2014-10-17 11:04:22 -0600

    Reformat

--- src/Core/Matlab/matlabconverter.h
+++ src/Core/Matlab/matlabconverter.h
@@ -37,12 +37,12 @@
 #define CORE_MATLABIO_MATLABCONVERTER_H 1
 
 /*
- *  This class converts matlab matrices into SCIRun objects and vice versa.
+ *  This class converts Matlab matrices into SCIRun objects and vice versa.
  *  The class is more a collection of functions then a real object.
  *
  *  The functions in this class are an attempt to bridge between the C++ kind
- *  of Object Oriented data management, towards a more classical way orginazing
- *  data in an ensamble of arrays of differrnt types
+ *  of Object Oriented data management, towards a more classical way organizing
+ *  data in an ensemble of arrays of different types
  *
  *  As SCIRun was not designed to easily extract and insert data the conversion
  *  algorithms are far from perfect and will need constant updating. A different
@@ -51,17 +51,12 @@
  * 
  */
 
-/* 
- * SCIRun data types have a lot of different classes, hence we need to include
- * a large number of class definitions......
- */
-
 #include <Core/Matlab/matfilebase.h>
 #include <Core/Matlab/share.h>
 
 /*
  * CLASS DESCRIPTION
- * Class for dealing with the conversion of "matlab" objects into
+ * Class for dealing with the conversion of "Matlab" objects into
  * SCIRun objects.
  * 
  * MEMORY MODEL
@@ -97,35 +92,35 @@ class SCISHARE matlabconverter : public matfilebase
     // Functions for converting back and forward of 
     // Compatible function:
     // indicates whether a matrix can be converted, for this purpose only
-    // the header of the matlabmatrix is read into memory. The function also
-    // returns a inforamation string describing the object for use in the GUI
+    // the header of the Matlab matrix is read into memory. The function also
+    // returns a information string describing the object for use in the GUI
     // This function returns a value 0 if the object cannot be converted and
     // a positive number if it is compatible. The higher the number the more
-    // likely it is the users wants to read this matlab array. 
+    // likely it is the users wants to read this Matlab array. 
     // The latter classification is based on some simple rules, like matrices
     // are probably of the double format etc.
     //
     // mlArrayTO.... function:
-    // Convert a matlab array into a SCIRun object. If the object is compatible 
+    // Convert a Matlab array into a SCIRun object. If the object is compatible 
     // this function should deal with the conversion. Depending on the information
-    // inthe object, fields like the property manager will be filled out.
+    // in the object, fields like the property manager will be filled out.
     //
     // ....TOmlMatrix function:
     // Convert a SCIRun object into a matlabarray. This version will produce a 
     // pure numeric array, with only the numeric values, every other field will
-    // be stripped and does not reappear in matlab.
+    // be stripped and does not reappear in Matlab.
     //
     // ....TOmlArray function:
-    // Try to convert a SCIRun object as complete as possible into a matlab field
+    // Try to convert a SCIRun object as complete as possible into a Matlab field
     // properties are stored and as well axis names, units etc. This function will
-    // create a matlab structured arrazy, with each field representing different
+    // create a Matlab structured array, with each field representing different
     // parts of the original SCIRun object.
     //
     // Limitations:
     // PropertyManager:
     // Current implementation only allows for translation of strings. Any numeric
     // property like vectors and scalars are still ignored. (Limitation is partly
-    // due to inavailablity of type information in the property manager)
+    // due to unavailability of type information in the property manager)
     // Complex numbers:
     // Currently only the real parts of complex numbers are taken. SCIRun does not
     // support complex numbers!
@@ -140,7 +135,7 @@ class SCISHARE matlabconverter : public matfilebase
     inline void setdatatype(matlabarray::mitype dataformat);
     // Index base sets the index base used for indices in for example geometries
     inline void setindexbase(int indexbase);
-    // In a numericmatrix all data will be stripped and the data will be saved as
+    // In a numeric matrix all data will be stripped and the data will be saved as
     // a plain dense or sparse matrix.
     inline void setdisabletranspose(bool dt);
 
@@ -157,8 +152,8 @@ class SCISHARE matlabconverter : public matfilebase
     inline void prefermatrices();
 
     // Since Bundles can be bundled, a choice needs to be made whether structured
-    // matlab matrices should become bundles or if possible should be converted into
-    // matrices/nrrds or fields. In case prefer bundles is set, a matlab structure will
+    // Matlab matrices should become bundles or if possible should be converted into
+    // matrices/nrrds or fields. In case prefer bundles is set, a Matlab structure will
     // be decomposed into bundles of sub bundles and of nrrds and matrices. In case
     // prefersciobjects is set each structure is read and if it can be translated into
     // a sciobject it will be come a field, nrrd or matrix and only at the last
@@ -167,21 +162,22 @@ class SCISHARE matlabconverter : public matfilebase
 
     inline void preferbundles();
     inline void prefersciobjects();
-
+#ifdef SCIRUN4_CODE_TO_BE_ENABLED_LATER
     // SCIRun STRINGS
-    int sciStringCompatible(matlabarray &mlarray, std::string &infostring, bool postremarks = true);
-    void mlArrayTOsciString(matlabarray &mlmat,SCIRun::StringHandle &scistr);
-    void sciStringTOmlArray(SCIRun::StringHandle &scistr,matlabarray &mlmat);
-
+    int sciStringCompatible(const matlabarray& mlarray, std::string& infostring, bool postremarks = true);
+    void mlArrayTOsciString(const matlabarray& mlmat, SCIRun::StringHandle& scistr);
+    void sciStringTOmlArray(SCIRun::StringHandle scistr, matlabarray& mlmat);
+#endif
     // SCIRun MATRICES
-    int sciMatrixCompatible(matlabarray &mlarray, std::string &infostring, bool postremarks = true);
-    void mlArrayTOsciMatrix(matlabarray &mlmat,SCIRun::MatrixHandle &scimat);
-    void sciMatrixTOmlArray(SCIRun::MatrixHandle &scimat,matlabarray &mlmat);
+    int sciMatrixCompatible(const matlabarray& mlarray, std::string& infostring, bool postremarks = true);
+    void mlArrayTOsciMatrix(const matlabarray& mlmat, SCIRun::MatrixHandle& scimat);
+    void sciMatrixTOmlArray(SCIRun::MatrixHandle scimat, matlabarray &mlmat);
 
+#ifdef SCIRUN4_CODE_TO_BE_ENABLED_LATER
     // SCIRun NRRDS
-    int sciNrrdDataCompatible(matlabarray &mlarray, std::string &infostring, bool postremarks = true);
-    void mlArrayTOsciNrrdData(matlabarray &mlmat,SCIRun::NrrdDataHandle &scinrrd);
-    void sciNrrdDataTOmlArray(SCIRun::NrrdDataHandle &scinrrd, matlabarray &mlmat);
+    int sciNrrdDataCompatible(const matlabarray& mlarray, std::string& infostring, bool postremarks = true);
+    void mlArrayTOsciNrrdData(const matlabarray& mlmat, SCIRun::NrrdDataHandle& scinrrd);
+    void sciNrrdDataTOmlArray(SCIRun::NrrdDataHandle scinrrd, matlabarray& mlmat);
 
     // SCIRun BUNDLES 
     int sciBundleCompatible(matlabarray &mlarray, std::string &infostring, bool postremarks = true);
@@ -191,15 +187,15 @@ class SCISHARE matlabconverter : public matfilebase
     // SCIRun ColorMaps (Reader only)
     int sciColorMapCompatible(matlabarray &mlarray, std::string &infostring, bool postremarks = true);
     void mlArrayTOsciColorMap(matlabarray &mlmat,SCIRun::ColorMapHandle &scinrrd);
-
+#endif
     // SCIRun Fields/Meshes
-    int sciFieldCompatible(matlabarray mlarray,std::string &infostring, bool postremarks = true);
-    void mlArrayTOsciField(matlabarray mlarray,SCIRun::FieldHandle &scifield);
-    void sciFieldTOmlArray(SCIRun::FieldHandle &scifield,matlabarray &mlarray);
+    int sciFieldCompatible(const matlabarray& mlarray,std::string &infostring, bool postremarks = true);
+    void mlArrayTOsciField(const matlabarray& mlarray, SCIRun::FieldHandle& scifield);
+    void sciFieldTOmlArray(SCIRun::FieldHandle scifield, matlabarray& mlarray);
 
     // SUPPORT FUNCTIONS
     // Test whether the proposed name of a matlab matrix is valid.
-    bool isvalidmatrixname(std::string name);
+    bool isvalidmatrixname(const std::string& name);
     
   private:
 
@@ -207,9 +203,9 @@ class SCISHARE matlabconverter : public matfilebase
 
     SCIRun::ProgressReporter* pr_;
     
-    inline void error(std::string error);
-    inline void warning(std::string warning);
-    inline void remark(std::string remark);
+    inline void error(const std::string& error);
+    inline void warning(const std::string& warning);
+    inline void remark(const std::string& remark);
 
     // FUNCTION FOR TRANSLATING THE CONTENTS OF A MATRIX (THE NUMERIC PART OF THE DATA)
     void sciMatrixTOmlMatrix(SCIRun::MatrixHandle &scimat,matlabarray &mlmat);
