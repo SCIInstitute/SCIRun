0246a0a213bebb15a55682b7d072d6c24d86e947
Dan White <dwhite@sci.utah.edu>
2014-12-17 16:16:51 -0700

    Use actual hash code (boost::hash fixed adjacency error)

--- src/Core/Algorithms/Legacy/Fields/RefineMesh/EdgePairHash.h
+++ src/Core/Algorithms/Legacy/Fields/RefineMesh/EdgePairHash.h
@@ -6,7 +6,7 @@
    Copyright (c) 2009 Scientific Computing and Imaging Institute,
    University of Utah.
 
-   
+
    Permission is hereby granted, free of charge, to any person obtaining a
    copy of this software and associated documentation files (the "Software"),
    to deal in the Software without restriction, including without limitation
@@ -24,7 +24,7 @@
    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
    DEALINGS IN THE SOFTWARE.
-*/
+   */
 
 #ifndef CORE_ALGORITHMS_FIELDS_REFINEMESH_EDGEPAIRHASH_H
 #define CORE_ALGORITHMS_FIELDS_REFINEMESH_EDGEPAIRHASH_H 1
@@ -34,51 +34,18 @@
 #include <boost/unordered_map.hpp> 
 
 namespace SCIRun{
-		namespace Core{
-				namespace Algorithms{
-						namespace Fields{
+  namespace Core{
+    namespace Algorithms{
+      namespace Fields{
 
-struct edgepair_t
-		{
-      VMesh::index_type first;
-      VMesh::index_type second;
-    };
+        typedef std::pair<VMesh::index_type, VMesh::index_type> edgepair_t;
 
-struct edgepairequal
-{
-    bool operator()(const edgepair_t &a, const edgepair_t &b) const
-    {
-    return a.first == b.first && a.second == b.second;
+        typedef boost::unordered_map<index_type, index_type> hash_map_type;
+
+
+      }
     }
-};
-
-struct edgepairless
-{
-    bool operator()(const edgepair_t &a, const edgepair_t &b)
-    {
-    return less(a, b);
-    }
-    static bool less(const edgepair_t &a, const edgepair_t &b)
-    {
-    return a.first < b.first || a.first == b.first && a.second < b.second;
-    }
-};
-
-struct IndexHash {
-  static const size_t bucket_size = 4;
-  static const size_t min_buckets = 8;
-  
-  size_t operator()(const index_type &idx) const
-    { return (static_cast<size_t>(idx)); }
-  
-  bool operator()(const index_type &i1, const index_type &i2) const
-    { return (i1 < i2); }
-};
-
-
-typedef boost::unordered_map<index_type,index_type,IndexHash> hash_map_type;
-
-
-								}}}}
+  }
+}
 
 #endif
