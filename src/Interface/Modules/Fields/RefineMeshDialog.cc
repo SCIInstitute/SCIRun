2e8e143028f2478ba3bc942f42a9be6933d7ea50
Dan White <dwhite@sci.utah.edu>
2014-12-17 16:16:29 -0700

    Fix dialog

--- src/Interface/Modules/Fields/RefineMeshDialog.cc
+++ src/Interface/Modules/Fields/RefineMeshDialog.cc
@@ -24,7 +24,7 @@
    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
    DEALINGS IN THE SOFTWARE.
-*/
+   */
 
 #include <Interface/Modules/Fields/RefineMeshDialog.h>
 #include <Core/Algorithms/Legacy/Fields/RefineMesh/RefineMesh.h>
@@ -36,51 +36,55 @@ using namespace SCIRun::Dataflow::Networks;
 using namespace SCIRun::Core::Algorithms::Fields;
 
 namespace SCIRun{
- namespace Gui{
- class RefineMeshDialogImpl
- {
- public:
- RefineMeshDialogImpl()
- {
- typedef boost::bimap<std::string, std::string>::value_type strPair;
- refineNameLookup_.insert(strPair("Do not add constraint","all"));
- refineNameLookup_.insert(strPair("Do not refine nodes/elements with values less than isovalue","greaterthan"));
- refineNameLookup_.insert(strPair("Do not refine nodes/elements with values unequal to isovalue","equal"));
- refineNameLookup_.insert(strPair("Do not refine nodes/elements with values greater than isovalue","lessthan"));
- refineNameLookup_.insert(strPair("Do not refine any elements","none"));
- }
- boost::bimap<std::string, std::string> refineNameLookup_;
- };
- }}
+  namespace Gui{
+    class RefineMeshDialogImpl
+    {
+    public:
+      RefineMeshDialogImpl()
+      {
+        typedef boost::bimap<std::string, std::string>::value_type strPair;
+        refineNameLookup_.insert(strPair("Do not add constraint", "all"));
+        refineNameLookup_.insert(strPair("Do not refine nodes/elements with values less than isovalue", "greaterthan"));
+        refineNameLookup_.insert(strPair("Do not refine nodes/elements with values unequal to isovalue", "equal"));
+        refineNameLookup_.insert(strPair("Do not refine nodes/elements with values greater than isovalue", "lessthan"));
+        refineNameLookup_.insert(strPair("Do not refine any elements", "none"));
+      }
+      boost::bimap<std::string, std::string> refineNameLookup_;
+    };
+  }
+}
 RefineMeshDialog::RefineMeshDialog(const std::string& name, ModuleStateHandle state,
   QWidget* parent /* = 0 */)
   : ModuleDialogGeneric(state, parent),
-	impl_(new RefineMeshDialogImpl)
+  impl_(new RefineMeshDialogImpl)
 {
   setupUi(this);
   setWindowTitle(QString::fromStdString(name));
   fixSize();
-		
-	addComboBoxManager(constraintComboBox_, Parameters::AddConstraints, impl_->refineNameLookup_);
-	addComboBoxManager(refinementComboBox_, Parameters::RefineMethod);
-	addDoubleSpinBoxManager(isoValueSpinBox_, Parameters::IsoValue);
 
-	connect(constraintComboBox_, SIGNAL(activated(int)), this, SLOT(setIsoValueEnabled()));
+  addComboBoxManager(constraintComboBox_, Parameters::AddConstraints, impl_->refineNameLookup_);
+  addComboBoxManager(refinementComboBox_, Parameters::RefineMethod);
+  addDoubleSpinBoxManager(isoValueSpinBox_, Parameters::IsoValue);
+
+  connect(constraintComboBox_, SIGNAL(activated(int)), this, SLOT(setIsoValueEnabled()));
 }
 
 void RefineMeshDialog::pull()
 {
-//?
+  pull_newVersionToReplaceOld();
+  isoValueSpinBox_->setEnabled(constraintComboBox_->currentText().contains("isovalue"));
 }
 
 void RefineMeshDialog::setIsoValueEnabled()
 {
-		if(!pulling_) 
-		if(constraintComboBox_->currentIndex() != 0)
-		{
-				isoValueSpinBox_->setEnabled(true);
-				state_->getValue(Parameters::IsoValue).toDouble(); 
-		}
-		else 
-				isoValueSpinBox_->setEnabled(false); 
+  if (!pulling_)
+  {
+    if (constraintComboBox_->currentIndex() != 0)
+    {
+      isoValueSpinBox_->setEnabled(true);
+      state_->getValue(Parameters::IsoValue).toDouble();
+    }
+    else
+      isoValueSpinBox_->setEnabled(false);
+  }
 }
