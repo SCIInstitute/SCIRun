55478b9beed7777c4b4498dbad611044448c0f4a
Brig Bagley <brig@sci.utah.edu>
2015-02-11 15:59:00 -0700

    re-added variable name for testing

--- src/Core/Algorithms/Base/AlgorithmVariableNames.h
+++ src/Core/Algorithms/Base/AlgorithmVariableNames.h
@@ -48,6 +48,7 @@ public:
   static const AlgorithmParameterName Preconditioner;
   static const AlgorithmParameterName Filename;
   static const AlgorithmParameterName BuildConvergence;
+  static const AlgorithmParameterName ColorMapName;
   static const AlgorithmParameterName FileTypeList;
   static const AlgorithmParameterName FileExtension;
   static const AlgorithmParameterName FileTypeName;
