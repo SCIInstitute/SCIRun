a89845d5eb1f40c7735ebdcbe9571c97e767f102
Dan White <dwhite@sci.utah.edu>
2015-05-11 13:07:29 -0600

    Fixes #965

--- src/Interface/Modules/Fields/CreateLatVolDialog.h
+++ src/Interface/Modules/Fields/CreateLatVolDialog.h
@@ -46,11 +46,6 @@ public:
     SCIRun::Dataflow::Networks::ModuleStateHandle state,
     QWidget* parent = 0);
   virtual void pull();
-
-private Q_SLOTS:
-  void push();
-private:
-  std::string getDataAtLocation() const;
 };
 
 }
