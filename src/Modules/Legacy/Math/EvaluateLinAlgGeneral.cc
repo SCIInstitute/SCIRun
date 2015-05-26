13e06301f0cb0632e18ffbedf343fc03da4daeb4
Dan White <dwhite@sci.utah.edu>
2015-05-26 16:02:29 -0600

    Working on module

--- src/Modules/Legacy/Math/EvaluateLinAlgGeneral.cc
+++ src/Modules/Legacy/Math/EvaluateLinAlgGeneral.cc
@@ -29,12 +29,14 @@
 #include <Modules/Legacy/Math/EvaluateLinAlgGeneral.h>
 #include <Core/Datatypes/String.h>
 #include <Core/Datatypes/Matrix.h>
+#include <Core/Algorithms/Base/AlgorithmVariableNames.h>
 //#include <Core/Parser/LinAlgEngine.h>
 
 //#include <Core/Util/StringUtil.h>
 
 using namespace SCIRun::Modules::Math;
 using namespace SCIRun::Core::Datatypes;
+using namespace SCIRun::Core::Algorithms;
 using namespace SCIRun::Dataflow::Networks;
 
 const ModuleLookupInfo EvaluateLinearAlgebraGeneral::staticInfo_("EvaluateLinearAlgebraGeneral", "Math", "SCIRun");
@@ -64,7 +66,7 @@ DECLARE_MAKER(EvaluateLinAlgGeneral)
 #endif
 
 EvaluateLinearAlgebraGeneral::EvaluateLinearAlgebraGeneral() : Module(staticInfo_)
-    //guifunction_(get_ctx()->subVar("function"), "o1 = i1 * i2;")
+    //guifunction_(get_ctx()->subVar("function"), )
 {
   INITIALIZE_PORT(i1);
   INITIALIZE_PORT(i2);
@@ -80,26 +82,23 @@ EvaluateLinearAlgebraGeneral::EvaluateLinearAlgebraGeneral() : Module(staticInfo
 
 void EvaluateLinearAlgebraGeneral::setStateDefaults()
 {
+  get_state()->setValue(Variables::FunctionString, std::string("o1 = i1 * i2;"));
 }
 
 void
 EvaluateLinearAlgebraGeneral::execute()
 {
-#if 0
-  std::vector<MatrixHandle> imatrix(5);
-  std::vector<MatrixHandle> omatrix(5);
+  const int NUM_PORTS = 5;
+  auto inputPorts = { i1, i2, i3, i4, i5 }; // TODO: call base class function?
+  std::vector<MatrixHandle> imatrix(NUM_PORTS);
+  std::vector<MatrixHandle> omatrix(NUM_PORTS);
   
-  for (int i=0; i<5; i++)
+  for (const auto& input : inputPorts)
   {
-    std::string matrixname = std::string("i")+to_string(i+1);
-    get_input_handle(matrixname,imatrix[i],false);
+    imatrix.push_back(getOptionalInput(input));
   }
   
-  TCLInterface::eval(get_id()+" update_text");
-
-  if (inputs_changed_ || guifunction_.changed() || !oport_cached("o1")
-        || !oport_cached("o2") || !oport_cached("o3")
-        || !oport_cached("o4") || !oport_cached("o5"))
+  if (needToExecute())
   {
     // Inform module that execution started
     update_state(Executing);
@@ -115,22 +114,23 @@ EvaluateLinearAlgebraGeneral::execute()
         if(!(engine.add_output_matrix(matrixname))) return;
       }
       
-      if (imatrix[p].get_rep())
+      if (imatrix[p])
       {
         std::string matrixname = std::string("i")+to_string(p+1);
         if (!(engine.add_input_matrix(matrixname,imatrix[p]))) return;
       }
     }
     
-    std::string function = guifunction_.get();
+    std::string function = get_state()->getValue(Variables::FunctionString).toString(),
 
-    if(!(engine.add_expressions(function))) return;
+    if(!engine.add_expressions(function)) 
+      return;
 
     // Actual engine call, which does the dynamic compilation, the creation of the
     // code for all the objects, as well as inserting the function and looping 
     // over every data point
 
-    if (!(engine.run()))
+    if (!engine.run())
     {
       ///-----------------------
       // Backwards compatibility with version 3.0.2
@@ -144,17 +144,16 @@ EvaluateLinearAlgebraGeneral::execute()
       return;
     }
     
-    for (size_t p = 0; p < 5; p++)
+    for (size_t p = 0; p < NUM_PORTS; p++)
     {
       std::string matrixname = std::string("o")+to_string(p+1);
-      engine.get_matrix(matrixname,omatrix[p]);
+      engine.get_matrix(matrixname, omatrix[p]);
     }
 
-    send_output_handle("o1", omatrix[0]);
-    send_output_handle("o2", omatrix[1]);
-    send_output_handle("o3", omatrix[2]);
-    send_output_handle("o4", omatrix[3]);
-    send_output_handle("o5", omatrix[4]);
+    sendOutput(o1, omatrix[0]);
+    sendOutput(o2, omatrix[1]);
+    sendOutput(o3, omatrix[2]);
+    sendOutput(o4, omatrix[3]);
+    sendOutput(o5, omatrix[4]);
   }
-#endif
 }
