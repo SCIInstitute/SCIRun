c77da312c01313695e8ccfce081c37d90ca3bd94
Dan White <dwhite@sci.utah.edu>
2015-07-06 16:55:29 -0600

    Module compiles

--- src/Modules/Legacy/Fields/TransformMeshWithTransform.h
+++ src/Modules/Legacy/Fields/TransformMeshWithTransform.h
@@ -6,7 +6,7 @@
    Copyright (c) 2015 Scientific Computing and Imaging Institute,
    University of Utah.
 
-   
+
    Permission is hereby granted, free of charge, to any person obtaining a
    copy of this software and associated documentation files (the "Software"),
    to deal in the Software without restriction, including without limitation
@@ -43,12 +43,14 @@ namespace SCIRun {
       public:
         TransformMeshWithTransform();
 
-        virtual void execute();
-        virtual void setStateDefaults() {}
+        virtual void execute() override;
+        virtual void setStateDefaults() override {}
 
         INPUT_PORT(0, InputField, LegacyField);
         INPUT_PORT(1, TransformMatrix, Matrix);
         OUTPUT_PORT(0, Transformed_Field, LegacyField);
+
+        static const Dataflow::Networks::ModuleLookupInfo staticInfo_;
       };
 
     }
