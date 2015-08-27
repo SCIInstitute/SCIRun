a5f46c602b5273e47fd9509f195f92e8079f5e19
Dan White <dwhite@sci.utah.edu>
2015-08-26 19:27:31 -0600

    Add module to factory

--- src/Modules/Factory/ModuleFactoryImpl1.cc
+++ src/Modules/Factory/ModuleFactoryImpl1.cc
@@ -74,6 +74,7 @@ DEALINGS IN THE SOFTWARE.
 #include <Modules/Legacy/Fields/ConvertFieldBasis.h>
 #include <Modules/Legacy/Fields/ScaleFieldMeshAndData.h>
 #include <Modules/Legacy/Fields/ProjectPointsOntoMesh.h>
+#include <Modules/Legacy/Fields/ClipFieldByMesh.h>
 #include <Modules/Legacy/Fields/CalculateDistanceToField.h>
 #include <Modules/Legacy/Fields/CalculateDistanceToFieldBoundary.h>
 #include <Modules/Legacy/Fields/MapFieldDataOntoElems.h>
@@ -225,6 +226,8 @@ void ModuleDescriptionLookup::addEssentialModules()
   addModuleDesc<GeneratePointSamplesFromFieldOrWidget>("Real ported module", "in progress");
   addModuleDesc<GenerateSinglePointProbeFromField>("Real ported module", "in progress");
   //addModuleDesc<ShowAndEditDipoles>("Real ported module", "in progress");
+
+  addModuleDesc<ClipFieldByMesh>("Real ported module", "...");
 
   // insert module desc here
 }
