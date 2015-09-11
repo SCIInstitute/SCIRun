29d4d240905862fa1a886ab86400811d84867e52
Dan White <dwhite@sci.utah.edu>
2015-09-10 18:18:18 -0600

    Add another one. PROBLEM: need a way to force regeneration of code file

--- src/Modules/Factory/ModuleFactoryImpl1.cc
+++ src/Modules/Factory/ModuleFactoryImpl1.cc
@@ -176,7 +176,7 @@ void ModuleDescriptionLookup::addEssentialModules()
   //addModuleDesc<TDCSSimulatorModule>("tDCSSimulator", "FiniteElements", "SCIRun", "Dummy module for design purposes", "...");
   addModuleDesc<SolveMinNormLeastSqSystem>("SolveMinNormLeastSqSystem", "Math", "SCIRun", "Real ported module", "...");
   addModuleDesc<CreateStandardColorMap>("In progress: four color maps available", "...");
-  addModuleDesc<GetDomainBoundary>("Real ported module: Many bugs and UI logic issues", "...");
+  //addModuleDesc<GetDomainBoundary>("Real ported module: Many bugs and UI logic issues", "...");
   addModuleDesc<JoinFields>("Real ported module: Many bugs and UI logic issues", "...");
   addModuleDesc<GetSliceFromStructuredFieldByIndices>("Real ported module", "...");
   addModuleDesc<CreateFieldData>("Real ported module", "...");
