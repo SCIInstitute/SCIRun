84cfc372e813c7ec5a6e0a7dad22d1ce89fb1b06
jessdtate <jess@sci.utah.edu>
2015-11-23 11:45:55 -0700

    added the module and algorithm files. need changes still.

---
+++ src/Modules/Math/ReportMatrixSliceMeasure.cc
@@ -0,0 +1,77 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2015 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Core/Datatypes/Matrix.h>
+#include <Dataflow/Network/Ports/MatrixPort.h>
+#include <Dataflow/Network/Module.h>
+#include <Core/Algorithms/Math/MathAlgo.h>
+
+namespace SCIRun {
+
+/// @class ReportMatrixRowMeasure
+/// @brief This module computes a measure on each row of the input matrix and
+/// stores the result in the output matrix. 
+
+class ReportMatrixRowMeasure : public Module {
+public:
+  ReportMatrixRowMeasure(GuiContext*);
+  virtual void execute();
+
+private:
+  GuiString guimethod_;  
+};
+
+
+DECLARE_MAKER(ReportMatrixRowMeasure)
+ReportMatrixRowMeasure::ReportMatrixRowMeasure(GuiContext* ctx)
+  : Module("ReportMatrixRowMeasure", ctx, Source, "Math", "SCIRun"),
+    guimethod_(ctx->subVar("method"))
+{
+}
+
+void
+ReportMatrixRowMeasure::execute()
+{
+  MatrixHandle input, output;
+  
+  if (!(get_input_handle("Matrix",input,true))) return;
+
+  if (inputs_changed_ || guimethod_.changed() || !oport_cached("Vector"))
+  {
+    SCIRunAlgo::MathAlgo algo(this);
+    
+    std::string method = guimethod_.get();
+    if (!(algo.ApplyRowOperation(input,output,method))) return;
+    
+    send_output_handle("Vector", output);
+  }
+}
+
+} // End namespace SCIRun
+
+
