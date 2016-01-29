6865000c0d61dbb9d8c48b7abadb61e5221cd3f2
Dan White <dwhite@sci.utah.edu>
2016-01-29 09:59:14 -0700

    Fix bug with dense matrix reading

--- src/Dataflow/Engine/Controller/PythonImpl.cc
+++ src/Dataflow/Engine/Controller/PythonImpl.cc
@@ -374,7 +374,7 @@ namespace
           }
 
           auto v = state->getValue(apn);
-        
+
   //TODO: extract and use for state get/set
           /// @todo: extract
           if ( const int* p = boost::get<int>( &v.value() ) )
@@ -405,7 +405,7 @@ namespace
             return boost::python::object(transient_value_cast<double>(v));
           if (transient_value_check<bool>(v))
             return boost::python::object(transient_value_cast<bool>(v));
-          
+
           return boost::python::object();
         }
       }
@@ -548,7 +548,7 @@ namespace
                   throw std::invalid_argument("Attempted to convert into dense matrix but row lengths are not all equal.");
                 for (int j = 0; j < len(row); ++j)
                 {
-                  (*dense)(i,j) = boost::python::extract<double>(row[i]);
+                  (*dense)(i,j) = boost::python::extract<double>(row[j]);
                 }
               }
             }
@@ -612,7 +612,7 @@ boost::shared_ptr<PyModule> PythonImpl::addModule(const std::string& name)
     std::cout << "Module added: " + m->get_id().id_ << std::endl;
   else
     std::cout << "Module add failed, no such module type" << std::endl;
-  
+
   return modules_[m->get_id().id_];
 }
 
