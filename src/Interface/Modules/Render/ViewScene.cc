0e3a60a40db15b5f7f740d2ac4e26b715641d814
basisunus <basisunus@yahoo.com>
2016-02-17 14:38:34 -0700

    added the matrix

--- src/Interface/Modules/Render/ViewScene.cc
+++ src/Interface/Modules/Render/ViewScene.cc
@@ -28,6 +28,7 @@ DEALINGS IN THE SOFTWARE.
 
 #include <gl-platform/GLPlatform.hpp>
 
+#include <Core/Datatypes/DenseMatrix.h>
 #include <Interface/Modules/Render/ViewScenePlatformCompatibility.h>
 #include <Interface/Modules/Render/ES/SRInterface.h>
 #include <Interface/Modules/Render/GLWidget.h>
@@ -954,9 +955,14 @@ void ViewSceneDialog::sendGeometryFeedbackToState(int x, int y)
   Variable::List geomInfo;
   geomInfo.push_back(makeVariable("xClick", x));
   geomInfo.push_back(makeVariable("yClick", y));
-  //DenseMatrixHandle matrixHandle;
-  //TODO:
-  geomInfo.push_back(Variable(Name("transform"), nullptr, Variable::DATATYPE_VARIABLE));
+  std::shared_ptr<SRInterface> spire = mSpire.lock();
+  DenseMatrixHandle matrixHandle(new DenseMatrix(4, 4));
+  glm::mat4 trans = spire->getWidgetTransform().transform;
+  (*matrixHandle) << trans[0][0], trans[1][0], trans[2][0], trans[3][0]
+    , trans[0][1], trans[1][1], trans[2][1], trans[3][1]
+    , trans[0][2], trans[1][2], trans[2][2], trans[3][2]
+    , trans[0][3], trans[1][3], trans[2][3], trans[3][3];
+  geomInfo.push_back(Variable(Name("transform"), matrixHandle, Variable::DATATYPE_VARIABLE));
   auto var = makeVariable("geomInfo", geomInfo);
   state_->setTransientValue(Parameters::GeometryFeedbackInfo, var);
 }
