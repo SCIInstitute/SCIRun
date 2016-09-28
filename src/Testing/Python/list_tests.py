0d47c43a1b35d27c47e02f410d6729c2e25aea57
Dan White <dwhite@sci.utah.edu>
2016-09-28 16:11:25 -0600

    Working on path issues

--- src/Testing/Python/list_tests.py
+++ src/Testing/Python/list_tests.py
@@ -2,24 +2,23 @@ import unittest
 import sys
 DEBUG = True
 
-def list_tests_from(path):
+def list_tests_from(base, path):
     loader = unittest.TestLoader()
-    suite = loader.discover(path)
-    print("Path: " + path)
+    suite = loader.discover(base + path)
+    #print("Path: " + path)
     #print("Suite: " + suite)
     for t in suite:
         tests = t._tests
-        print("Tests: " + tests)
-        if len(tests):
-            for test in tests:
-                if DEBUG:
-                    print("Test: " + test)
-                for btest in test._tests:
-                    btestname = btest.__str__().split()
-                    print("Btestname: " + btestname)
-                    #print(path + "." + btestname[1][1:-1] + "." + btestname[0])
+        #print("Tests: " + tests)
+        for test in tests:
+            #if DEBUG:
+                #print("Test: " + test)
+            for btest in test._tests:
+                btestname = btest.__str__().split()
+                #print("Btestname: " + btestname[1] + " -- " + btestname[0])
+                print(path + "." + btestname[1][1:-1] + "." + btestname[0])
 
 if __name__ == "__main__": 
   # Include the directories
-  list_tests_from(sys.argv[1] + "Unit")
-  list_tests_from(sys.argv[1] + "Regression")
+  list_tests_from(sys.argv[1], "Unit")
+  list_tests_from(sys.argv[1], "Regression")
