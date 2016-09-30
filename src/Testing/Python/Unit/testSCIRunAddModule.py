6f9c9c9a7a02f698345e088c2dc80f0b1416fe90
Dan White <dwhite@sci.utah.edu>
2016-09-30 12:19:10 -0600

    Finally got the environment variable from cmake to test

--- src/Testing/Python/Unit/testSCIRunAddModule.py
+++ src/Testing/Python/Unit/testSCIRunAddModule.py
@@ -1,6 +1,7 @@
 import unittest
 import subprocess
 import os
+import sys
 
 def getSourceRoot():
     return os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
@@ -11,19 +12,16 @@ def getNetworkFolder():
 def getSCIRunExecutable():
     return os.path.join(os.path.dirname(getSourceRoot()), "bin", "SCIRun", "Release", "SCIRun.exe")
 
-    
+
 class TestSCIRunAddModule(unittest.TestCase):
-    executable = "SCIRun"
+    executable = "SCIRUN"
     def test_scirun_module_ids(self):
-        self.assertEqual(self.executable, r"C:\Dev\SCIRun\bin\SCIRun\Release\SCIRun.exe")
+        self.assertEqual(self.executable, "C:/_/SCIRun/Release/SCIRun.exe")
         self.assertEqual(getSCIRunExecutable(), r"C:\Dev\SCIRun\bin\SCIRun\Release\SCIRun.exe")
         self.assertEqual(getSourceRoot(), r"C:\Dev\SCIRun\src")
         self.assertEqual(getNetworkFolder(), r"C:\Dev\SCIRun\src\ExampleNets\regression")
         subprocess.check_output([self.executable, os.path.join(getNetworkFolder(), "basicViz.srn5"), "-E", "--no_splash"])
         #subprocess.check_output([r"C:\_\SCIRun\Release\SCIRun.exe", r"C:\Dev\SCIRun\src\ExampleNets\regression\setuptdcs_colin27_patchelc.srn5", "-E", "--no_splash"])
-        
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        TestSCIRunAddModule.executable = sys.argv.pop()
-    unittest.main()
+       
+if __name__.startswith("Unit"):
+  TestSCIRunAddModule.executable = os.environ["SCIRUN_EXECUTABLE_PATH_FOR_PYTHON_TESTS"]
--- src/Testing/Python/list_tests.py
+++ src/Testing/Python/list_tests.py
@@ -5,20 +5,14 @@ DEBUG = True
 def list_tests_from(base, path):
     loader = unittest.TestLoader()
     suite = loader.discover(base + path)
-    #print("Path: " + path)
-    #print("Suite: " + suite)
     for t in suite:
         tests = t._tests
-        #print("Tests: " + tests)
         for test in tests:
-            #if DEBUG:
-                #print("Test: " + test)
             for btest in test._tests:
                 btestname = btest.__str__().split()
-                #print("Btestname: " + btestname[1] + " -- " + btestname[0])
                 print(path + "." + btestname[1][1:-1] + "." + btestname[0])
 
 if __name__ == "__main__": 
   # Include the directories
   list_tests_from(sys.argv[1], "Unit")
-  list_tests_from(sys.argv[1], "Regression")
+  #list_tests_from(sys.argv[1], "Regression")
