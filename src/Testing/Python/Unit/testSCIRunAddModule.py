3ba7b21c8adf7f5495751824321a5cf43718b84c
Dan White <dwhite@sci.utah.edu>
2016-10-03 10:51:01 -0600

    Clean up

--- src/Testing/Python/Unit/testSCIRunAddModule.py
+++ src/Testing/Python/Unit/testSCIRunAddModule.py
@@ -1,7 +1,6 @@
 import unittest
 import subprocess
 import os
-import sys
 
 def getSourceRoot():
     return os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
@@ -9,15 +8,11 @@ def getSourceRoot():
 def getNetworkFolder():
     return os.path.join(getSourceRoot(), "ExampleNets", "regression")
 
-def getSCIRunExecutable():
-    return os.path.join(os.path.dirname(getSourceRoot()), "bin", "SCIRun", "Release", "SCIRun.exe")
-
 
 class TestSCIRunAddModule(unittest.TestCase):
     executable = "SCIRUN"
     def test_scirun_module_ids(self):
         self.assertEqual(self.executable, "C:/_/SCIRun/Release/SCIRun.exe")
-        self.assertEqual(getSCIRunExecutable(), r"C:\Dev\SCIRun\bin\SCIRun\Release\SCIRun.exe")
         self.assertEqual(getSourceRoot(), r"C:\Dev\SCIRun\src")
         self.assertEqual(getNetworkFolder(), r"C:\Dev\SCIRun\src\ExampleNets\regression")
         subprocess.check_output([self.executable, os.path.join(getNetworkFolder(), "basicViz.srn5"), "-E", "--no_splash"])
