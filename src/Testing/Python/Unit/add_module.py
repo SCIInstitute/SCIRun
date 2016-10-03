550fd42b1e66436cdc814fa77d39b9ba414beb87
Dan White <dwhite@sci.utah.edu>
2016-10-03 11:18:29 -0600

    Refactor with lambda

--- src/Testing/Python/Unit/add_module.py
+++ src/Testing/Python/Unit/add_module.py
@@ -1,10 +1,12 @@
-import sys
+def scirun_assert(func):
+  if not func():
+    scirun_add_module("ReadMatrix") # guarantee errored network
+    scirun_quit_after_execute()
+    scirun_execute_all()
+  else:
+    scirun_force_quit() # SCIRun returns 0 to console
 
 m1 = scirun_add_module("ReadMatrix")
 m2 = scirun_add_module("ReportMatrixInfo")
+scirun_assert(lambda: len(scirun_module_ids()) == 2)
 
-if len(scirun_module_ids()) != 2:
-  scirun_quit_after_execute()
-  scirun_execute_all()
-else:
-  scirun_force_quit()
