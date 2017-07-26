4881f7d1e0e58c3ed6e4dea03ae1fc2b4a4dd212
Dan White <dwhite@sci.utah.edu>
2017-07-26 15:07:37 -0600

    External project for GLM

--- Superbuild/Superbuild.cmake
+++ Superbuild/Superbuild.cmake
@@ -169,6 +169,7 @@ ADD_EXTERNAL( ${SUPERBUILD_DIR}/SQLiteExternal.cmake SQLite_external )
 ADD_EXTERNAL( ${SUPERBUILD_DIR}/LibPNGExternal.cmake LibPNG_external )
 ADD_EXTERNAL( ${SUPERBUILD_DIR}/TeemExternal.cmake Teem_external )
 ADD_EXTERNAL( ${SUPERBUILD_DIR}/FreetypeExternal.cmake Freetype_external )
+ADD_EXTERNAL( ${SUPERBUILD_DIR}/GLMExternal.cmake GLM_external )
 
 IF(BUILD_WITH_PYTHON)
   ADD_EXTERNAL( ${SUPERBUILD_DIR}/PythonExternal.cmake Python_external )
@@ -219,6 +220,7 @@ SET(SCIRUN_CACHE_ARGS
     "-DTeem_DIR:PATH=${Teem_DIR}"
     "-DTetgen_DIR:PATH=${Tetgen_DIR}"
     "-DFreetype_DIR:PATH=${Freetype_DIR}"
+	"-DGLM_DIR:PATH=${GLM_DIR}"
     "-DSCI_DATA_DIR:PATH=${SCI_DATA_DIR}"
 )
 
