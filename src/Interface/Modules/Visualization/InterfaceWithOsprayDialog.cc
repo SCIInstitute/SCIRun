6db1e55846ca77befa4554f2ca9cf607ed39740f
Dan White <dwhite@sci.utah.edu>
2017-09-05 14:33:50 -0600

    Add dummy ospray module

---
+++ src/Interface/Modules/Visualization/InterfaceWithOsprayDialog.cc
@@ -0,0 +1,110 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2015 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   License for the specific language governing rights and limitations under
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Interface/Modules/Visualization/InterfaceWithOsprayDialog.h>
+#include <Modules/Visualization/InterfaceWithOspray.h>
+#include <Dataflow/Network/ModuleStateInterface.h>
+
+using namespace SCIRun::Gui;
+using namespace SCIRun::Dataflow::Networks;
+using namespace SCIRun::Core::Datatypes;
+using namespace SCIRun::Modules::Visualization;
+
+//using SCM = SCIRun::Modules::Visualization::ShowColorMap;
+
+InterfaceWithOsprayDialog::InterfaceWithOsprayDialog(const std::string& name, ModuleStateHandle state,
+  QWidget* parent /* = 0 */)
+  : ModuleDialogGeneric(state, parent)
+{
+  setupUi(this);
+  setWindowTitle(QString::fromStdString(name));
+  fixSize();
+	// addRadioButtonGroupManager({ leftRadioButton_, bottomRadioButton_ }, SCM::DisplaySide);
+	// addRadioButtonGroupManager({ firstHalfRadioButton_, fullRadioButton_, secondHalfRadioButton_ }, SCM::DisplayLength);
+  // addSpinBoxManager(textSizeSpinner_, SCM::TextSize);
+	// addSpinBoxManager(ticksSpinner_, SCM::Labels);
+	// addDoubleSpinBoxManager(scaleSpinner_, SCM::Scale);
+	// addLineEditManager(unitsText_, SCM::Units);
+	// addSpinBoxManager(sigDigitsSpinner_, SCM::SignificantDigits);
+  //
+	// addSpinBoxManager(xTranslationSpin_, SCM::XTranslation);
+	// addSpinBoxManager(yTranslationSpin_, SCM::YTranslation);
+  //
+  // connect(textColorPushButton_, SIGNAL(clicked()), this, SLOT(getColor()));
+  // connectButtonsToExecuteSignal({ leftRadioButton_, bottomRadioButton_, firstHalfRadioButton_, fullRadioButton_, secondHalfRadioButton_, textColorPushButton_ } );
+  //
+  // addDoubleSpinBoxManager(&r_, SCM::TextRed);
+  // addDoubleSpinBoxManager(&g_, SCM::TextGreen);
+  // addDoubleSpinBoxManager(&b_, SCM::TextBlue);
+  //
+  // if (state_->getValue(SCM::TextRed).toDouble() < 0)
+  // {
+  //   text_color_ = QColor(255, 255, 255, 255);
+  //   r_.setValue(1.);
+  //   g_.setValue(1.);
+  //   b_.setValue(1.);
+  //   sigDigitsSpinner_->setValue(2);
+  //   textSizeSpinner_->setValue(8);
+  //   ticksSpinner_->setValue(10);
+  //   scaleSpinner_->setValue(1.);
+  //   xTranslationSpin_->setValue(0);
+  //   yTranslationSpin_->setValue(0);
+  // }
+  // else
+  // {
+  //   text_color_ = QColor(state_->getValue(SCM::TextRed).toDouble() * 255,
+  //     state_->getValue(SCM::TextGreen).toDouble() * 255,
+  //     state_->getValue(SCM::TextBlue).toDouble() * 255);
+  //   std::stringstream ss;
+  //   ss << "background-color: rgb(" << text_color_.red() << ", " <<
+  //     text_color_.green() << ", " << text_color_.blue() << ");";
+  //   textColorDisplayLabel_->setStyleSheet(QString::fromStdString(ss.str()));
+  // }
+  //
+  // createExecuteInteractivelyToggleAction();
+}
+/*
+void ShowColorMapDialog::pullSpecial()
+{
+  r_.setValue(state_->getValue(SCM::TextRed).toDouble());
+  g_.setValue(state_->getValue(SCM::TextGreen).toDouble());
+  b_.setValue(state_->getValue(SCM::TextBlue).toDouble());
+}
+
+void ShowColorMapDialog::getColor()
+{
+  text_color_ = QColorDialog::getColor(text_color_, this, "Choose text color");
+  std::stringstream ss;
+  ss << "background-color: rgb(" << text_color_.red() << ", " <<
+    text_color_.green() << ", " << text_color_.blue() << ");";
+  textColorDisplayLabel_->setStyleSheet(QString::fromStdString(ss.str()));
+  r_.setValue(text_color_.redF());
+  g_.setValue(text_color_.greenF());
+  b_.setValue(text_color_.blueF());
+}
+*/
---
+++ src/Modules/Visualization/InterfaceWithOspray.h
@@ -0,0 +1,120 @@
+/*
+For more information, please see: http://software.sci.utah.edu
+
+The MIT License
+
+Copyright (c) 2015 Scientific Computing and Imaging Institute,
+University of Utah.
+
+License for the specific language governing rights and limitations under
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef MODULES_VISUALIZATION_INTERFACEWITHOSPRAY_H
+#define MODULES_VISUALIZATION_INTERFACEWITHOSPRAY_H
+
+#include <Dataflow/Network/GeometryGeneratingModule.h>
+#include <Core/Thread/Interruptible.h>
+#include <Modules/Visualization/share.h>
+
+namespace SCIRun {
+
+  namespace Core
+  {
+    namespace Algorithms
+    {
+      namespace Visualization
+      {
+        ALGORITHM_PARAMETER_DECL(CylinderRadius);
+      }
+    }
+  }
+
+  namespace Modules {
+    namespace Visualization {
+
+      class SCISHARE InterfaceWithOspray : public Dataflow::Networks::GeometryGeneratingModule,
+        public Has2InputPorts<FieldPortTag, ColorMapPortTag>,
+        public Has1OutputPort<GeometryPortTag>,
+        public Core::Thread::Interruptible
+      {
+      public:
+        InterfaceWithOspray();
+        virtual void execute() override;
+
+        // static const Core::Algorithms::AlgorithmParameterName FieldName;
+        // static const Core::Algorithms::AlgorithmParameterName NodesAvailable;
+        // static const Core::Algorithms::AlgorithmParameterName EdgesAvailable;
+        // static const Core::Algorithms::AlgorithmParameterName FacesAvailable;
+        // static const Core::Algorithms::AlgorithmParameterName ShowNodes;
+        // static const Core::Algorithms::AlgorithmParameterName ShowEdges;
+        // static const Core::Algorithms::AlgorithmParameterName ShowFaces;
+        // static const Core::Algorithms::AlgorithmParameterName NodeTransparency;
+        // static const Core::Algorithms::AlgorithmParameterName EdgeTransparency;
+        // static const Core::Algorithms::AlgorithmParameterName FaceTransparency;
+        // static const Core::Algorithms::AlgorithmParameterName FaceInvertNormals;
+        // static const Core::Algorithms::AlgorithmParameterName NodeAsPoints;
+        // static const Core::Algorithms::AlgorithmParameterName NodeAsSpheres;
+        // static const Core::Algorithms::AlgorithmParameterName EdgesAsLines;
+        // static const Core::Algorithms::AlgorithmParameterName EdgesAsCylinders;
+        // static const Core::Algorithms::AlgorithmParameterName DefaultMeshColor;
+        // static const Core::Algorithms::AlgorithmParameterName FaceTransparencyValue;
+        // static const Core::Algorithms::AlgorithmParameterName EdgeTransparencyValue;
+        // static const Core::Algorithms::AlgorithmParameterName NodeTransparencyValue;
+        // static const Core::Algorithms::AlgorithmParameterName FacesColoring;
+        // static const Core::Algorithms::AlgorithmParameterName NodesColoring;
+        // static const Core::Algorithms::AlgorithmParameterName EdgesColoring;
+        // static const Core::Algorithms::AlgorithmParameterName SphereScaleValue;
+        // static const Core::Algorithms::AlgorithmParameterName CylinderResolution;
+        // static const Core::Algorithms::AlgorithmParameterName SphereResolution;
+        // static const Core::Algorithms::AlgorithmParameterName CylinderRadius;
+        // static const Core::Algorithms::AlgorithmParameterName DefaultTextColor;
+        // static const Core::Algorithms::AlgorithmParameterName ShowText;
+        // static const Core::Algorithms::AlgorithmParameterName ShowDataValues;
+        // static const Core::Algorithms::AlgorithmParameterName ShowNodeIndices;
+        // static const Core::Algorithms::AlgorithmParameterName ShowEdgeIndices;
+        // static const Core::Algorithms::AlgorithmParameterName ShowFaceIndices;
+        // static const Core::Algorithms::AlgorithmParameterName ShowCellIndices;
+        // static const Core::Algorithms::AlgorithmParameterName CullBackfacingText;
+        // static const Core::Algorithms::AlgorithmParameterName TextAlwaysVisible;
+        // static const Core::Algorithms::AlgorithmParameterName RenderAsLocation;
+        // static const Core::Algorithms::AlgorithmParameterName TextSize;
+        // static const Core::Algorithms::AlgorithmParameterName TextPrecision;
+        // static const Core::Algorithms::AlgorithmParameterName TextColoring;
+        // static const Core::Algorithms::AlgorithmParameterName UseFaceNormals;
+
+
+        INPUT_PORT(0, Field, Field);
+        INPUT_PORT(1, ColorMapObject, ColorMap);
+        OUTPUT_PORT(0, SceneGraph, GeometryObject);
+
+        MODULE_TRAITS_AND_INFO(ModuleHasUI)
+
+        virtual void setStateDefaults() override;
+      private:
+
+      };
+
+
+
+    } // Visualization
+  } // Modules
+} // SCIRun
+
+#endif
