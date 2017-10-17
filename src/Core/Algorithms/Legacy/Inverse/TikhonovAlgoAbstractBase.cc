b491b378f43980f80af906acd40e46616783fc5d
Dan White <dwhite@sci.utah.edu>
2017-10-17 11:55:05 -0600

    Start reformatting

--- src/Core/Algorithms/Legacy/Inverse/TikhonovAlgoAbstractBase.cc
+++ src/Core/Algorithms/Legacy/Inverse/TikhonovAlgoAbstractBase.cc
@@ -115,101 +115,83 @@ TikhonovAlgoAbstractBase::TikhonovAlgoAbstractBase()
 ////// CHECK IF INPUT MATRICES HAVE THE CORRECT SIZE
 bool TikhonovAlgoAbstractBase::checkInputMatrixSizes( const AlgorithmInput & input) const
 {
-	// get inputs
 	auto forwardMatrix_ = input.get<Matrix>(ForwardMatrix);
 	auto measuredData_ = input.get<Matrix>(MeasuredPotentials);
 	auto sourceWeighting_ = input.get<Matrix>(WeightingInSourceSpace);
 	auto sensorWeighting_ = input.get<Matrix>(WeightingInSensorSpace);
 
-	// get input sizes
-    const int M = forwardMatrix_->nrows();
-    const int N = forwardMatrix_->ncols();
+  const int M = forwardMatrix_->nrows();
+  const int N = forwardMatrix_->ncols();
 
-    // check that rows of fwd matrix equal number of measurements
-    if ( M != measuredData_->nrows() )
+  // check that rows of fwd matrix equal number of measurements
+  if ( M != measuredData_->nrows() )
+  {
+  	THROW_ALGORITHM_INPUT_ERROR("Input matrix dimensions must agree.");
+  	return false;
+  }
+
+  // check source regularization matrix sizes
+  if (sourceWeighting_)
+  {
+    if( get(Parameters::regularizationSolutionSubcase).toInt()==solution_constrained )
     {
-		THROW_ALGORITHM_INPUT_ERROR("Input matrix dimensions must agree.");
-		return false;
+      // check that the matrix is of appropriate size (equal number of rows as columns in fwd matrix)
+      if ( N != sourceWeighting_->ncols() )
+      {
+    		THROW_ALGORITHM_INPUT_ERROR("Solution Regularization Matrix must have the same number of rows as columns in the Forward Matrix !");
+    		return false;
+      }
     }
-
-    // // check that number of time samples is 1. @JCOLLFONT to change for a more general case later (should add a for loop)
-    // if (1 != measuredData_->ncols())
-    // {
-	// 	THROW_ALGORITHM_INPUT_ERROR("Measured data must be a vector");
-	// 	return false;
-    // }
-
-    // check source regularization matrix sizes
-    if (sourceWeighting_)
+    // otherwise, if the source regularization is provided as the squared version (RR^T)
+    else if ( get(Parameters::regularizationSolutionSubcase).toInt()==solution_constrained_squared )
     {
-        if( get(Parameters::regularizationSolutionSubcase).toInt()==solution_constrained )
-        {
-            // check that the matrix is of appropriate size (equal number of rows as columns in fwd matrix)
-            if ( N != sourceWeighting_->ncols() )
-            {
-				THROW_ALGORITHM_INPUT_ERROR("Solution Regularization Matrix must have the same number of rows as columns in the Forward Matrix !");
-				return false;
-            }
-        }
-        // otherwise, if the source regularization is provided as the squared version (RR^T)
-        else if ( get(Parameters::regularizationSolutionSubcase).toInt()==solution_constrained_squared )
-        {
-            // check that the matrix is of appropriate size and squared (equal number of rows as columns in fwd matrix)
-            if ( ( N != sourceWeighting_->nrows() ) || ( N != sourceWeighting_->ncols() ) )
-            {
-				THROW_ALGORITHM_INPUT_ERROR("The squared solution Regularization Matrix must have the same number of rows and columns and must be equal to the number of columns in the Forward Matrix !");
-				return false;
-            }
-        }
+      // check that the matrix is of appropriate size and squared (equal number of rows as columns in fwd matrix)
+      if ( ( N != sourceWeighting_->nrows() ) || ( N != sourceWeighting_->ncols() ) )
+      {
+        THROW_ALGORITHM_INPUT_ERROR("The squared solution Regularization Matrix must have the same number of rows and columns and must be equal to the number of columns in the Forward Matrix !");
+	      return false;
+      }
     }
+  }
 
-    // check measurement regularization matrix sizes
-    if (sensorWeighting_)
+  // check measurement regularization matrix sizes
+  if (sensorWeighting_)
+  {
+    if (get(Parameters::regularizationResidualSubcase).toInt() == residual_constrained)
     {
-        if (get(Parameters::regularizationResidualSubcase).toInt() == residual_constrained)
-        {
-            // check that the matrix is of appropriate size (equal number of rows as rows in fwd matrix)
-            if(M != sensorWeighting_->ncols())
-            {
-				THROW_ALGORITHM_INPUT_ERROR("Data Residual Weighting Matrix must have the same number of rows as the Forward Matrix !");
-				return false;
-            }
-        }
-        // otherwise if the source covariance matrix is provided in squared form
-        else if  ( get(Parameters::regularizationResidualSubcase).toInt() == residual_constrained_squared )
-        {
-            // check that the matrix is of appropriate size and squared (equal number of rows as rows in fwd matrix)
-            if( (M != sensorWeighting_->nrows()) || (M != sensorWeighting_->ncols()) )
-            {
-				THROW_ALGORITHM_INPUT_ERROR("Squared data Residual Weighting Matrix must have the same number of rows and columns as number of rows in the Forward Matrix !");
-				return false;
-			}
-
-        }
+      // check that the matrix is of appropriate size (equal number of rows as rows in fwd matrix)
+      if(M != sensorWeighting_->ncols())
+      {
+        THROW_ALGORITHM_INPUT_ERROR("Data Residual Weighting Matrix must have the same number of rows as the Forward Matrix !");
+        return false;
+      }
     }
-
-    return true;
-
+    // otherwise if the source covariance matrix is provided in squared form
+    else if  ( get(Parameters::regularizationResidualSubcase).toInt() == residual_constrained_squared )
+    {
+      // check that the matrix is of appropriate size and squared (equal number of rows as rows in fwd matrix)
+      if( (M != sensorWeighting_->nrows()) || (M != sensorWeighting_->ncols()) )
+      {
+        THROW_ALGORITHM_INPUT_ERROR("Squared data Residual Weighting Matrix must have the same number of rows and columns as number of rows in the Forward Matrix !");
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
-
-/////////////////////////
-/////////  run()
 AlgorithmOutput TikhonovAlgoAbstractBase::run(const AlgorithmInput & input) const
 {
-	// get inputs
 	auto forwardMatrix_ = input.get<Matrix>(TikhonovAlgoAbstractBase::ForwardMatrix);
 	auto measuredData_ = input.get<Matrix>(TikhonovAlgoAbstractBase::MeasuredPotentials);
 	auto sourceWeighting_ = input.get<Matrix>(TikhonovAlgoAbstractBase::WeightingInSourceSpace);
 	auto sensorWeighting_ = input.get<Matrix>(TikhonovAlgoAbstractBase::WeightingInSensorSpace);
 
-	// get Parameters
 	auto RegularizationMethod_gotten = getOption(Parameters::RegularizationMethod);
 	auto TikhonovImplementation_gotten = get(Parameters::TikhonovImplementation).toString();
 
-    // Alocate Variable
 	DenseMatrix solution;
-    double lambda = 0;
+  double lambda = 0;
 	double lambda_ = 0;
 
 	// check input MATRICES
@@ -317,14 +299,8 @@ AlgorithmOutput TikhonovAlgoAbstractBase::run(const AlgorithmInput & input) cons
 	output[RegularizationParameter] = boost::make_shared<DenseMatrix>(lambda_);
 
 	return output;
-
 }
-//////// fi  run()
-///////////////////////////
 
-
-///////////////////////////
-/////// compute L-curve
 double TikhonovAlgoAbstractBase::computeLcurve( const SCIRun::Core::Algorithms::Inverse::TikhonovImpl * algoImpl, const AlgorithmInput & input ) const
 {
 	// get inputs
@@ -332,172 +308,108 @@ double TikhonovAlgoAbstractBase::computeLcurve( const SCIRun::Core::Algorithms::
 	auto measuredData_ = input.get<Matrix>(TikhonovAlgoAbstractBase::MeasuredPotentials);
 	auto sourceWeighting_ = input.get<Matrix>(TikhonovAlgoAbstractBase::WeightingInSourceSpace);
 	auto sensorWeighting_ = input.get<Matrix>(TikhonovAlgoAbstractBase::WeightingInSensorSpace);
-    // define the step size of the lambda vector to be computed  (distance between min and max divided by number of desired lambdas in log scale)
-    const int nLambda = get(Parameters::LambdaNum).toInt();
+  // define the step size of the lambda vector to be computed  (distance between min and max divided by number of desired lambdas in log scale)
+  const int nLambda = get(Parameters::LambdaNum).toInt();
 	const double lambdaMin_ = get(Parameters::LambdaMin).toDouble();
 	const double lambdaMax_ = get(Parameters::LambdaMax).toDouble();
 	double lambda = 0;
 
 	// prealocate vector of lambdas and eta and rho
-    std::vector<double> rho(nLambda, 0.0);
-    std::vector<double> eta(nLambda, 0.0);
+  std::vector<double> rho(nLambda, 0.0);
+  std::vector<double> eta(nLambda, 0.0);
 
-	// Compute the lambda array
-	std::vector<double> lambdaArray(nLambda, 0.0);
-    lambdaArray = algoImpl->computeLambdaArray( lambdaMin_, lambdaMax_, nLambda );
+  auto lambdaArray = algoImpl->computeLambdaArray( lambdaMin_, lambdaMax_, nLambda );
 
+  DenseMatrix CAx, Rx;
+  DenseMatrix solution;
 
-    DenseMatrix CAx, Rx;
-    DenseMatrix solution;
+  lambdaArray[0] = lambdaMin_;
 
-    lambdaArray[0] = lambdaMin_;
+  // for all lambdas
+  for (int j = 0; j < nLambda; j++)
+  {
+    solution = algoImpl->computeInverseSolution( lambdaArray[j], false);
 
-    // for all lambdas
-    for (int j = 0; j < nLambda; j++)
+    // if using source regularization matrix, apply it to compute Rx (for the eta computations)
+    if (sourceWeighting_)
     {
-
-        // COMPUTE INVERSE SOLUTION
-        solution = algoImpl->computeInverseSolution( lambdaArray[j], false);
-
-
-        // if using source regularization matrix, apply it to compute Rx (for the eta computations)
-        if (sourceWeighting_)
-        {
-            if (solution.nrows() == sourceWeighting_->ncols()) // check that regularization matrix and solution match sizes
-                Rx = (*castMatrix::toDense(sourceWeighting_)) * solution;
-            else
-            {
-				BOOST_THROW_EXCEPTION(AlgorithmProcessingException() << ErrorMessage(" Solution weighting matrix unexpectedly does not fit to compute the weighted solution norm. "));
-            }
-        }
-        else
-            Rx = solution;
-
-
-        auto Ax = (*castMatrix::toDense(forwardMatrix_)) * solution;
-        auto residualSolution = Ax - (*castMatrix::toDense(measuredData_));
-
-        // if using source regularization matrix, apply it to compute Rx (for the eta computations)
-        if (sensorWeighting_)
-            CAx = (*castMatrix::toDense(sensorWeighting_)) * residualSolution;
-        else
-            CAx = residualSolution;
-
-
-        // compute rho and eta. Using Frobenious norm when using matrices
-		rho[j] = CAx.norm();
-		eta[j] = Rx.norm();
-
+      if (solution.nrows() == sourceWeighting_->ncols()) // check that regularization matrix and solution match sizes
+        Rx = (*castMatrix::toDense(sourceWeighting_)) * solution;
+      else
+      {
+        BOOST_THROW_EXCEPTION(AlgorithmProcessingException() << ErrorMessage(" Solution weighting matrix unexpectedly does not fit to compute the weighted solution norm. "));
+      }
     }
+    else
+      Rx = solution;
 
-    // Find corner in L-curve
-    lambda = FindCorner( rho, eta, lambdaArray, nLambda );
+    auto Ax = (*castMatrix::toDense(forwardMatrix_)) * solution;
+    auto residualSolution = Ax - (*castMatrix::toDense(measuredData_));
 
-    // // update GUI
-    // if (updateLCurveGui_)
-    //     updateLCurveGui_(lambda, *lcurveInput_handle_, lambda_index);
+    // if using source regularization matrix, apply it to compute Rx (for the eta computations)
+    if (sensorWeighting_)
+      CAx = (*castMatrix::toDense(sensorWeighting_)) * residualSolution;
+    else
+      CAx = residualSolution;
 
-    // return lambda
-    return lambda;
+    // compute rho and eta. Using Frobenious norm when using matrices
+    rho[j] = CAx.norm();
+    eta[j] = Rx.norm();
+  }
 
+  // Find corner in L-curve
+  lambda = FindCorner( rho, eta, lambdaArray, nLambda );
+
+  // TODO: update GUI
+
+  return lambda;
 }
-//////// fi compute L-curve
-/////////////////////////////
-
 
 ///// Find Corner, find the maximal curvature which corresponds to the L-curve corner
 double TikhonovAlgoAbstractBase::FindCorner( const std::vector<double>& rho, const std::vector<double>& eta, const std::vector<double>& lambdaArray, const int nLambda )
 {
-	int lambda_index;
-    std::vector<double> deta(nLambda);
-    std::vector<double> ddeta(nLambda);
-    std::vector<double> drho(nLambda);
-    std::vector<double> ddrho(nLambda);
-    std::vector<double> lrho(nLambda);
-    std::vector<double> leta(nLambda);
-    DenseColumnMatrix kapa(nLambda);
+  std::vector<double> deta(nLambda);
+  std::vector<double> ddeta(nLambda);
+  std::vector<double> drho(nLambda);
+  std::vector<double> ddrho(nLambda);
+  std::vector<double> lrho(nLambda);
+  std::vector<double> leta(nLambda);
+  DenseColumnMatrix kapa(nLambda);
 
-    double maxKapa = -1.0e10;
-    for (int i = 0; i < nLambda; i++)
+  double maxKapa = -1.0e10;
+  for (int i = 0; i < nLambda; i++)
+  {
+    lrho[i] = std::log10(rho[i]);
+    leta[i] = std::log10(eta[i]);
+    if(i>0)
     {
-        lrho[i] = std::log10(rho[i]);
-        leta[i] = std::log10(eta[i]);
-        if(i>0)
-        {
-            deta[i] = (leta[i]-leta[i-1]) / (lambdaArray[i]-lambdaArray[i-1]); // compute first derivative
-            drho[i] = (lrho[i]-lrho[i-1]) / (lambdaArray[i]-lambdaArray[i-1]);
-        }
-        if(i>1)
-        {
-            ddeta[i] = (deta[i]-deta[i-1]) / (lambdaArray[i]-lambdaArray[i-1]); // compute second derivative from first
-            ddrho[i] = (drho[i]-drho[i-1]) / (lambdaArray[i]-lambdaArray[i-1]);
-        }
+      deta[i] = (leta[i]-leta[i-1]) / (lambdaArray[i]-lambdaArray[i-1]); // compute first derivative
+      drho[i] = (lrho[i]-lrho[i-1]) / (lambdaArray[i]-lambdaArray[i-1]);
     }
-    drho[0] = drho[1];
-    deta[0] = deta[1];
-    ddrho[0] = ddrho[2];
-    ddrho[1] = ddrho[2];
-    ddeta[0] = ddeta[2];
-    ddeta[1] = ddeta[2];
-
-    lambda_index = 0;
-    for (int i = 0; i < nLambda; i++)
+    if(i>1)
     {
-        kapa[i] = std::abs((drho[i] * ddeta[i] - ddrho[i] * deta[i]) /  //compute curvature
-                           std::sqrt(std::pow((deta[i]*deta[i]+drho[i]*drho[i]), 3.0)));
-        if (kapa[i] > maxKapa) // find max curvature
-        {
-            maxKapa = kapa[i];
-            lambda_index = i;
-        }
+      ddeta[i] = (deta[i]-deta[i-1]) / (lambdaArray[i]-lambdaArray[i-1]); // compute second derivative from first
+      ddrho[i] = (drho[i]-drho[i-1]) / (lambdaArray[i]-lambdaArray[i-1]);
     }
+  }
+  drho[0] = drho[1];
+  deta[0] = deta[1];
+  ddrho[0] = ddrho[2];
+  ddrho[1] = ddrho[2];
+  ddeta[0] = ddeta[2];
+  ddeta[1] = ddeta[2];
 
-    return lambdaArray[lambda_index];
+  int lambda_index = 0;
+  for (int i = 0; i < nLambda; i++)
+  {
+    kapa[i] = std::abs((drho[i] * ddeta[i] - ddrho[i] * deta[i]) /  //compute curvature
+                       std::sqrt(std::pow((deta[i]*deta[i]+drho[i]*drho[i]), 3.0)));
+    if (kapa[i] > maxKapa) // find max curvature
+    {
+      maxKapa = kapa[i];
+      lambda_index = i;
+    }
+  }
+
+  return lambdaArray[lambda_index];
 }
-
-// ///// Search for closest Lambda to given lambda
-// double TikhonovAlgoAbstractBase::LambdaLookup( LCurveInput& Linput, double lambda, int& lambda_index, const double epsilon)
-// {
-// 	const int nLambda = Linput.nLambda_;
-// 	const std::vector<double>& lambdaArray = Linput.lambdaArray_;
-//
-//     for (int i = 0; i < nLambda-1; ++i)
-//     {
-//         if (i > 0 && (lambda < lambdaArray[i-1] || lambda > lambdaArray[i+1])) continue;
-//
-//         double lambda_step_midpoint = std::abs(lambdaArray[i+1] - lambdaArray[i])/2;
-//
-//         if (std::abs(lambda - lambdaArray[i]) <= epsilon)  // TODO: is this a reasonable comparison???
-//         {
-//             lambda_index = i;
-//             return lambdaArray[lambda_index];
-//         }
-//
-//         if (std::abs(lambda - lambdaArray[i]) < lambda_step_midpoint)
-//         {
-//             lambda_index = i;
-//             return lambdaArray[lambda_index];
-//         }
-//
-//         if (std::abs(lambda - lambdaArray[i+1]) < lambda_step_midpoint)
-//         {
-//             lambda_index = i+1;
-//             return lambdaArray[lambda_index];
-//         }
-//     }
-//     return -1;
-// }
-
-// ////////// update L-curve graph
-// void TikhonovAlgoAbstractBase::update_graph( LCurveInput & input, double lambda, int lambda_index, const double epsilon)
-// {
-//
-//     if (lcurveInput_handle_ && input.updateLCurveGui_)
-//     {
-//         lambda = LambdaLookup(*lcurveInput_handle_, lambda, lambda_index, epsilon);
-//         if (lambda >= 0)
-//         {
-//             input.updateLCurveGui_(lambda, *lcurveInput_handle_, lambda_index);
-//         }
-//     }
-// }
