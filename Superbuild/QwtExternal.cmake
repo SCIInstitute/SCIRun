aa0cbe8257d391b995c2b3f9428de76db3e203c4
Dan White <dwhite@sci.utah.edu>
2018-05-03 15:00:59 -0600

    Switch Qwt branch

--- Superbuild/QwtExternal.cmake
+++ Superbuild/QwtExternal.cmake
@@ -25,7 +25,7 @@
 #  DEALINGS IN THE SOFTWARE.
 
 SET_PROPERTY(DIRECTORY PROPERTY "EP_BASE" ${ep_base})
-SET(qwt_GIT_TAG "origin/static-build")
+SET(qwt_GIT_TAG "origin/qt5-static")
 
 # If CMake ever allows overriding the checkout command or adding flags,
 # git checkout -q will silence message about detached head (harmless).
@@ -39,7 +39,7 @@ ExternalProject_Add(Qwt_external
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=${CMAKE_VERBOSE_MAKEFILE}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
-    -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}
+    -DQt5_PATH:PATH=${Qt5_PATH}
 )
 
 ExternalProject_Get_Property(Qwt_external BINARY_DIR)
