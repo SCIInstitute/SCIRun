5a9bde76d8841e73ebca7c83fa7d5164de6b0868
chhabragarima <chhabragarima15@gmail.com>
2018-07-05 16:23:59 -0600

    License comments updated for examplesand CreateStandardMatrix

--- src/Interface/Modules/Math/CreateStandardMatrixDialog.h
+++ src/Interface/Modules/Math/CreateStandardMatrixDialog.h
@@ -3,7 +3,7 @@
  The MIT License
  Copyright (c) 2015 Scientific Computing and Imaging Institute,
  University of Utah.
- License for the specific language governing rights and limitations under
+ 
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
  to deal in the Software without restriction, including without limitation
