51d969c2d076c56840ab2138bd4e835fce315c27
Dan White <dwhite@sci.utah.edu>
2019-03-05 15:15:16 -0700

    Start working on log test module

---
+++ src/Modules/Basic/LoggingTester.cc
@@ -0,0 +1,52 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2015 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+#include <iostream>
+#include <Modules/Basic/LoggingTester.h>
+#include <Core/Datatypes/DenseMatrix.h>
+
+using namespace SCIRun::Modules::Basic;
+using namespace SCIRun::Dataflow::Networks;
+using namespace SCIRun::Core::Datatypes;
+using namespace SCIRun::Core::Algorithms;
+
+MODULE_INFO_DEF(LoggingTester, Testing, SCIRun)
+
+LoggingTester::LoggingTester() : Module(staticInfo_, false)
+{
+  INITIALIZE_PORT(Input);
+}
+
+void LoggingTester::execute()
+{
+  std::cout << "std::cout message" << std::endl;
+}
+
+void LoggingTester::setStateDefaults()
+{
+  
+}
