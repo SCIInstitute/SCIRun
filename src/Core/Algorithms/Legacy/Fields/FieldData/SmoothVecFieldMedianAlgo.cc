e9c4183dfac83b84d7d4a1aee8cc32a4ac297f70
Dan White <dwhite@sci.utah.edu>
2019-04-11 10:32:31 -0600

    Algo compiles

--- src/Core/Algorithms/Legacy/Fields/FieldData/SmoothVecFieldMedianAlgo.cc
+++ src/Core/Algorithms/Legacy/Fields/FieldData/SmoothVecFieldMedianAlgo.cc
@@ -6,7 +6,7 @@
    Copyright (c) 2015 Scientific Computing and Imaging Institute,
    University of Utah.
 
-   
+
    Permission is hereby granted, free of charge, to any person obtaining a
    copy of this software and associated documentation files (the "Software"),
    to deal in the Software without restriction, including without limitation
@@ -26,170 +26,182 @@
    DEALINGS IN THE SOFTWARE.
 */
 
-#include <Core/Algorithms/Fields/FieldData/SmoothVecFieldMedian.h>
-#include <Core/Datatypes/FieldInformation.h>
-#include <Core/Datatypes/SparseRowMatrix.h>
-#include <Core/Datatypes/FieldInformation.h>
-#include <Core/Geometry/Vector.h>
+#include <Core/Algorithms/Legacy/Fields/FieldData/SmoothVecFieldMedianAlgo.h>
+#include <Core/Datatypes/Legacy/Field/FieldInformation.h>
+#include <Core/Datatypes/Legacy/Field/Mesh.h>
+#include <Core/Datatypes/Legacy/Field/VMesh.h>
+#include <Core/Datatypes/Legacy/Field/VField.h>
+//#include <Core/Datatypes/SparseRowMatrix.h>
+#include <Core/GeometryPrimitives/Vector.h>
 #include <Core/Math/MiscMath.h>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-#include <iostream>
 
-namespace SCIRunAlgo {
+#include <Core/Algorithms/Base/AlgorithmPreconditions.h>
+#include <Core/Algorithms/Base/AlgorithmVariableNames.h>
 
 using namespace SCIRun;
+using namespace SCIRun::Core::Algorithms;
+using namespace SCIRun::Core::Algorithms::Fields;
+using namespace SCIRun::Core::Datatypes;
+using namespace SCIRun::Core::Geometry;
 
-bool
-SmoothVecFieldMedianAlgo::run(FieldHandle input, FieldHandle& output)
+bool SmoothVecFieldMedianAlgo::runImpl(FieldHandle input, FieldHandle& output) const
 {
-  algo_start("SmoothVecFieldMedian");
-  if (input.get_rep() == 0)
+  ScopedAlgorithmStatusReporter asr(this, "SmoothVecFieldMedian");
+
+  if (!input)
   {
     error("No input field");
-    algo_end(); return (false);
+    return false;
   }
-  
+
   FieldInformation fi(input);
 
   if (fi.is_nodata())
   {
     error("Input field does not have data associated with it");
-    algo_end(); return (false);    
+    return false;
   }
 
-  if (!(fi.is_vector()))
+  if (!fi.is_vector())
   {
     error("The data needs to be of vector type");
-    algo_end(); return (false);    
+    return false;
   }
 
-  output = CreateField(fi,input->mesh());
+  output = CreateField(fi, input->mesh());
 
-  if (output.get_rep() == 0)
+  if (!output)
   {
     error("Could not allocate output field");
-    algo_end(); return (false);      
+    return false;
   }
 
   VField* ifield = input->vfield();
   VField* ofield = output->vfield();
   VMesh* imesh = input->vmesh();
-  VMesh::Elem::index_type a,b;
-  Vector v0,v1,v2,v3;  
+  VMesh::Elem::index_type a, b;
+  Vector v0, v1, v2, v3;
   std::vector<double> angles, original;
-  int middle=0, not_on_list=0, myloc=0;
-  double gdot=0, m1=0, m2=0, angle=0;
-   
-  
-  
+  int middle = 0, myloc = 0;
+  bool not_on_list = false;
+  double gdot = 0, m1 = 0, m2 = 0, angle = 0;
 
   if (ifield->is_vector())
   {
     VField::size_type num_values = ifield->num_values();
-  
+
     imesh->synchronize(Mesh::ELEM_NEIGHBORS_E);
 
-   
-    
-    
-  
     int cnt = 0;
-   // for (VField::index_type idx = 0; idx < 2000; idx++)
+    
     for (VMesh::Elem::index_type idx = 0; idx < num_values; idx++)
     {
       //calculate neighborhoods
-    VMesh::Elem::array_type nci, ncitot, Nlist;
-    VMesh::Elem::array_type nci2;
-      //cerr<<"one loop"<<idx<<endl;
+      VMesh::Elem::array_type nci, ncitot, Nlist;
+      VMesh::Elem::array_type nci2;
       imesh->get_neighbors(nci, idx);
-      
-      ncitot.push_back(idx);
-      
-      for (size_t t=0; t< nci.size(); t++)
-      {
-         ncitot.push_back(nci[t]);
-         imesh->get_neighbors(nci2, nci[t]);
-         
-         for (size_t L=0; L< nci2.size(); L++)
-         {            
-           ncitot.push_back(nci2[L]);
-         } 
-         
-         for (size_t t2=0; t2< nci2.size(); t2++)
-         {
-           VMesh::Elem::array_type nci3;
 
-           imesh->get_neighbors(nci3, nci2[t2]);
-           for (size_t L=0; L< nci3.size(); L++)
-           {            
-             ncitot.push_back(nci3[L]);
-           }         
-         }
-      }
-      
-      not_on_list=0;
-      for (size_t p1=0; p1< ncitot.size(); p1++)
+      ncitot.push_back(idx);
+
+      for (size_t t = 0; t < nci.size(); t++)
       {
-        not_on_list=0;
-        for (size_t p2=0; p2< Nlist.size(); p2++)  
-        {  
-          if(ncitot[p1]==Nlist[p2])
-          {
-            not_on_list=1;
-          }
-          
+        ncitot.push_back(nci[t]);
+        imesh->get_neighbors(nci2, nci[t]);
+
+        for (size_t L = 0; L < nci2.size(); L++)
+        {
+          ncitot.push_back(nci2[L]);
         }
-        if(not_on_list==0)
+
+        for (size_t t2 = 0; t2 < nci2.size(); t2++)
+        {
+          VMesh::Elem::array_type nci3;
+
+          imesh->get_neighbors(nci3, nci2[t2]);
+          for (size_t L = 0; L < nci3.size(); L++)
+          {
+            ncitot.push_back(nci3[L]);
+          }
+        }
+      }
+
+      not_on_list = false;
+      for (size_t p1 = 0; p1 < ncitot.size(); p1++)
+      {
+        not_on_list = false;
+        for (size_t p2 = 0; p2 < Nlist.size(); p2++)
+        {
+          if (ncitot[p1] == Nlist[p2])
+          {
+            not_on_list = true;
+          }
+
+        }
+        if (!not_on_list)
         {
           Nlist.push_back(ncitot[p1]);
         }
       }
-            
+
       angles.clear();
       original.clear();
-      ifield->get_value(v0,idx);
-      for (size_t q=0; q< Nlist.size(); q++)
+      ifield->get_value(v0, idx);
+      for (size_t q = 0; q < Nlist.size(); q++)
       {
-        a=Nlist[q];
-        ifield->get_value(v1,a);
-        if(v0.length()*v1.length()==0)
+        a = Nlist[q];
+        ifield->get_value(v1, a);
+        if (v0.length()*v1.length() == 0)
         {
           angles.push_back(0);
           original.push_back(0);
         }
         else
         {
-          gdot=Dot(v0,v1);
-          m1=v0.length();
-          m2=v1.length();
-          angle=(gdot/(m1*m2));
+          gdot = Dot(v0, v1);
+          m1 = v0.length();
+          m2 = v1.length();
+          angle = (gdot / (m1*m2));
           angles.push_back(angle);
           original.push_back(angle);
         }
-      } 
-      
+      }
+
       sort(angles.begin(), angles.end());
-      middle=(int)((angles.size()+1)/2);
-      for(size_t k=0; k<original.size(); k++)
+      middle = (int)((angles.size() + 1) / 2);
+      for (size_t k = 0; k < original.size(); k++)
       {
-        if(original[k]==angles[middle])
+        if (original[k] == angles[middle])
         {
-          myloc=k;
-          k=original.size();
+          myloc = k;
+          k = original.size();
         }
       }
 
-      b=Nlist[myloc];
-      ifield->get_value(v2,b);
-           
-      ofield->set_value(v2,idx);
-      
-      cnt++; if (cnt == 200) { cnt = 0; update_progress(idx,num_values); }
+      b = Nlist[myloc];
+      ifield->get_value(v2, b);
+
+      ofield->set_value(v2, idx);
+
+      cnt++; 
+      if (cnt == 200) 
+      { 
+        cnt = 0; 
+        update_progress_max(idx, num_values); 
+      }
     }
   }
-  algo_end(); return (true);
+  return (true);
 }
 
-} // end namespace SCIRun
+AlgorithmOutput SmoothVecFieldMedianAlgo::run(const AlgorithmInput& input) const
+{
+  auto field = input.get<Field>(Variables::InputField);
+
+  FieldHandle outputField;
+  if (!runImpl(field, outputField))
+    THROW_ALGORITHM_PROCESSING_ERROR("False returned on legacy run call.");
+
+  AlgorithmOutput output;
+  output[Variables::OutputField] = outputField;
+  return output;
+}
