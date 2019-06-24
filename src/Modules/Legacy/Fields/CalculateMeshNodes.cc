fc58a45094ea53f3b7e44fd8a1f513bd56a42851
Daniel White <dwhite@sci.utah.edu>
2019-06-24 14:47:02 -0600

    Start working on CalcMeshNodes

--- src/Modules/Legacy/Fields/CalculateMeshNodes.cc
+++ src/Modules/Legacy/Fields/CalculateMeshNodes.cc
@@ -6,7 +6,7 @@
    Copyright (c) 2015 Scientific Computing and Imaging Institute,
    University of Utah.
 
-   
+
    Permission is hereby granted, free of charge, to any person obtaining a
    copy of this software and associated documentation files (the "Software"),
    to deal in the Software without restriction, including without limitation
@@ -26,24 +26,24 @@
    DEALINGS IN THE SOFTWARE.
 */
 
-// Include all code for the dynamic engine
+#include <Modules/Legacy/Fields/CalculateMeshNodes.h>
+//#include <Core/Algorithms/Base/AlgorithmBase.h>
+//#include <Core/Datatypes/Legacy/Field/Field.h>
+#include <Core/Datatypes/Scalar.h>
+//#include <Core/Algorithms/Legacy/Fields/MeshDerivatives/CalculateMeshCenterAlgo.h>
+
 #include <Core/Datatypes/String.h>
 #include <Core/Datatypes/Matrix.h>
-#include <Core/Datatypes/Field.h>
-#include <Core/Datatypes/FieldInformation.h>
+//#include <Core/Datatypes/Field.h>
+//#include <Core/Datatypes/FieldInformation.h>
 #include <Core/Parser/ArrayMathEngine.h>
-
-#include <Dataflow/Network/Module.h>
-#include <Dataflow/Network/Ports/MatrixPort.h>
-#include <Dataflow/Network/Ports/FieldPort.h>
-#include <Dataflow/Network/Ports/StringPort.h>
 
 namespace SCIRun {
 
 /// @class CalculateMeshNodes
 /// @brief Calculate new positions for the node locations of the mesh.
 
-class CalculateMeshNodes : public Module 
+class CalculateMeshNodes : public Module
 {
   public:
     CalculateMeshNodes(GuiContext*);
@@ -53,11 +53,11 @@ class CalculateMeshNodes : public Module
     virtual void tcl_command(GuiArgs&, void*);
     virtual void presave();
     virtual void post_read();
-    
+
 
   private:
     GuiString guifunction_;     // function code
-    
+
     bool old_version_;
 };
 
@@ -77,14 +77,14 @@ void CalculateMeshNodes::execute()
   StringHandle func;
   std::vector<MatrixHandle> matrices;
 
-  // Get the new input data:  
+  // Get the new input data:
   get_input_handle("Field",field,true);
   if (get_input_handle("Function",func,false))
   {
     if (func.get_rep())
     {
       guifunction_.set(func->get());
-      get_ctx()->reset();  
+      get_ctx()->reset();
     }
   }
   get_dynamic_input_handles("Array",matrices,false);
@@ -101,21 +101,21 @@ void CalculateMeshNodes::execute()
       error("This module cannot handle more than 23 input matrices.");
       return;
     }
- 
+
     NewArrayMathEngine engine;
     engine.set_progress_reporter(this);
- 
+
     FieldInformation fi(field);
     if (field->vfield()->basis_order() == 1 || fi.is_pointcloudmesh())
     {
-    
+
       // Create the DATA object for the function
       // DATA is the data on the field
-      if(!(engine.add_input_fielddata("DATA",field))) return; 
+      if(!(engine.add_input_fielddata("DATA",field))) return;
 
       //--------------------------
       //Backwards compatiblity with 3.0.2
-      if(!(engine.add_input_fielddata("v",field))) return; 
+      if(!(engine.add_input_fielddata("v",field))) return;
       //--------------------------
 
     }
@@ -123,39 +123,39 @@ void CalculateMeshNodes::execute()
     {
       remark("DATA is not defined as data is not located on the nodes");
     }
-    
-    // Create the POS, X,Y,Z, data location objects.  
+
+    // Create the POS, X,Y,Z, data location objects.
 
     if(!(engine.add_input_fieldnodes("POS",field))) return;
     if(!(engine.add_input_fieldnodes_coordinates("X","Y","Z",field))) return;
-    
+
     //--------------------------
     //Backwards compatiblity with 3.0.2
     if(!(engine.add_input_fieldnodes_coordinates("x","y","z",field))) return;
     //--------------------------
-    
+
 
     // Create the ELEMENT object describing element properties
     if (field->vfield()->basis_order() == 1)
     {
       if(!(engine.add_input_fielddata_element("ELEMENT",field,field->vfield()->basis_order()))) return;
     }
-    
+
     // Loop through all matrices and add them to the engine as well
     char mname = 'A';
     std::string matrixname("A");
-    
+
     for (size_t p = 0; p < numinputs; p++)
     {
       if (matrices[p].get_rep() == 0)
       {
         error("No matrix was found on input port.");
-        return;      
+        return;
       }
 
       matrixname[0] = mname++;
       if (!(engine.add_input_matrix(matrixname,matrices[p]))) return;
-    }    
+    }
 
     std::string function = guifunction_.get();
     bool has_NEWPOS = true;
@@ -169,9 +169,9 @@ void CalculateMeshNodes::execute()
       // Backwards compatibility with version 3.0.2
       if(!(engine.add_output_fieldnodes("result",field))) return;
       has_NEWPOS = false;
-      ///-----------------------    
+      ///-----------------------
     }
-    
+
     // Add an object for getting the index and size of the array.
 
     if(!(engine.add_index("INDEX"))) return;
@@ -181,7 +181,7 @@ void CalculateMeshNodes::execute()
     if(!(engine.add_expressions(function))) return;
 
     // Actual engine call, which does the dynamic compilation, the creation of the
-    // code for all the objects, as well as inserting the function and looping 
+    // code for all the objects, as well as inserting the function and looping
     // over every data point
 
     if (!(engine.run()))
@@ -196,22 +196,22 @@ void CalculateMeshNodes::execute()
         error("We are sorry for this inconvenience, but we do not longer support dynamically compiling code in SCIRun.");
       }
       ///-----------------------
-      
+
       return;
     }
 
     // Get the result from the engine
-    FieldHandle ofield;    
+    FieldHandle ofield;
     if(has_NEWPOS)
     {
       engine.get_field("NEWPOS",ofield);
     }
     else
     {
-      engine.get_field("result",ofield);    
+      engine.get_field("result",ofield);
     }
-    
-    // send new output if there is any: 
+
+    // send new output if there is any:
     send_output_handle("Field", ofield);
   }
 }
@@ -256,7 +256,7 @@ CalculateMeshNodes::post_read()
 
     const std::string modName = get_ctx()->getfullname() + "-";
     std::string val;
-  
+
     ///-----------------------
     // Backwards compatibility with intermediate version
 
@@ -265,7 +265,7 @@ CalculateMeshNodes::post_read()
       // Upgrade function so it will still function
       // In the old code, a casting operator needed to be
       // entered, this is ugly and the new code does not need
-      // it, it actually does not recognize it, and hence 
+      // it, it actually does not recognize it, and hence
       // we need to remove it
       std::string::size_type castop = val.find("(Point)");
       if (castop != std::string::npos)
@@ -298,5 +298,3 @@ CalculateMeshNodes::post_read()
 
 
 } // End namespace SCIRun
-
-
