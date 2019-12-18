684c6316e10460f0a1fbe52495da82422b615a52
Dan White <dwhite@sci.utah.edu>
2019-12-18 14:33:53 -0700

    Start enabling basic GenerateElectrode module for #1289

---
+++ src/Modules/Legacy/Fields/GenerateElectrode.h
@@ -0,0 +1,83 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2019 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+   */
+
+#ifndef MODULES_LEGACY_FIELDS_GENERATEELECTRODE_H
+#define MODULES_LEGACY_FIELDS_GENERATEELECTRODE_H
+
+//#include <Core/Datatypes/Geometry.h>
+//#include <Core/GeometryPrimitives/BBox.h>
+#include <Dataflow/Network/GeometryGeneratingModule.h>
+//#include <Graphics/Datatypes/GeometryImpl.h>
+//#include <Graphics/Widgets/Widget.h>
+//#include <Graphics/Widgets/WidgetFactory.h>
+#include <Modules/Legacy/Fields/share.h>
+
+
+namespace SCIRun {
+  namespace Modules {
+    namespace Fields {
+
+      class SCISHARE GenerateElectrode : public SCIRun::Dataflow::Networks::GeometryGeneratingModule,
+        public Has2InputPorts<FieldPortTag, MatrixPortTag>,
+        public Has4OutputPorts<FieldPortTag, GeometryPortTag, FieldPortTag, MatrixPortTag>
+      {
+      public:
+        GenerateElectrode();
+        void execute() override;
+        void setStateDefaults() override;
+
+        //static const Core::Algorithms::AlgorithmParameterName FieldName;
+        //static const Core::Algorithms::AlgorithmParameterName WidgetScaleFactor;
+        //static const Core::Algorithms::AlgorithmParameterName Sizing;
+        //static const Core::Algorithms::AlgorithmParameterName ShowLastAsVector;
+        //static const Core::Algorithms::AlgorithmParameterName ShowLines;
+        //static const Core::Algorithms::AlgorithmParameterName Reset;
+        //static const Core::Algorithms::AlgorithmParameterName MoveDipolesTogether;
+        //static const Core::Algorithms::AlgorithmParameterName DipolePositions;
+        //static const Core::Algorithms::AlgorithmParameterName DipoleDirections;
+        //static const Core::Algorithms::AlgorithmParameterName DipoleScales;
+        //static const Core::Algorithms::AlgorithmParameterName DataSaved;
+        //static const Core::Algorithms::AlgorithmParameterName LargestSize;
+
+        INPUT_PORT(0, InputField, Field);
+        INPUT_PORT(1, ParameterMatrix, DenseMatrix);
+        OUTPUT_PORT(0, OutputField, Field);
+        OUTPUT_PORT(1, ElectrodeWidget, GeometryObject);
+        OUTPUT_PORT(2, ControlPoints, Field);
+        OUTPUT_PORT(3, ParameterMatrix, DenseMatrix);
+
+        MODULE_TRAITS_AND_INFO(ModuleHasUI);
+
+      private:
+
+      };
+    }
+  }
+};
+
+#endif
