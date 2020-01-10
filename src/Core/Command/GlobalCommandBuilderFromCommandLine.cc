e17d3dc0c33d5d85355c4085db39ccbc301ba03d
Daniel White <dwhite@sci.utah.edu>
2020-01-10 11:34:27 -0700

    Closes #2003

--- src/Core/Command/GlobalCommandBuilderFromCommandLine.cc
+++ src/Core/Command/GlobalCommandBuilderFromCommandLine.cc
@@ -125,7 +125,8 @@ using namespace SCIRun::Core::Algorithms;
     }
     else if (params->disableGui() && !params->interactiveMode())
     {
-      std::cout << "No input files: run with GUI or in interactive mode (-i)" << std::endl;
+      if (!params->importNetworkFile())
+        std::cout << "No input files: run with GUI or in interactive mode (-i)" << std::endl;
       q->enqueue(cmdFactory_->create(GlobalCommands::QuitCommand));
     }
 
