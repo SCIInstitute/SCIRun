418970d809aed3e35a8aaa757cbae3e0247c0d75
nids2001 <nidhipatel@sci.utah.edu>
2020-01-15 13:54:55 -0700

    fix Core/CommanLine file headers

--- src/Core/CommandLine/Tests/CommandLineTests.cc
+++ src/Core/CommandLine/Tests/CommandLineTests.cc
@@ -3,10 +3,9 @@
 
    The MIT License
 
-   Copyright (c) 2015 Scientific Computing and Imaging Institute,
+   Copyright (c) 2020 Scientific Computing and Imaging Institute,
    University of Utah.
 
-   
    Permission is hereby granted, free of charge, to any person obtaining a
    copy of this software and associated documentation files (the "Software"),
    to deal in the Software without restriction, including without limitation
@@ -39,7 +38,7 @@ namespace
   {
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);    
+    po::notify(vm);
     return vm;
   }
 }
