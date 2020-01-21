af2d3d67a07e27425ae61f5a8174ed891383d189
nids2001 <nidhipatel@sci.utah.edu>
2020-01-20 17:31:00 -0700

    fix Core/Thread file headers

--- src/Core/Thread/Legacy/RecursiveMutex.h
+++ src/Core/Thread/Legacy/RecursiveMutex.h
@@ -3,10 +3,9 @@
 
    The MIT License
 
-   Copyright (c) 2015 Scientific Computing and Imaging Institute,
+   Copyright (c) 2020 Scientific Computing and Imaging Institute,
    University of Utah.
 
-   
    Permission is hereby granted, free of charge, to any person obtaining a
    copy of this software and associated documentation files (the "Software"),
    to deal in the Software without restriction, including without limitation
@@ -25,7 +24,6 @@
    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
    DEALINGS IN THE SOFTWARE.
 */
-
 
 
 ///
@@ -63,7 +61,7 @@ class RecursiveMutex_private;
  but must be matched with calls to <b>unlock()</b>.  This class
  may be less efficient that the <b>Mutex</b> class, and should not
  be used unless the recursive lock feature is really required.
- 
+
 ****************************************/
 class SCISHARE RecursiveMutex {
 public:
@@ -106,5 +104,3 @@ private:
 } // End namespace SCIRun
 
 #endif
-
-
