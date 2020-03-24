83b83fdbdb317d9c461d357e4345a3ec01792e73
RubioJr9 <u0893472@utah.edu>
2020-03-24 16:06:45 -0600

    License headers

--- src/Modules/Visualization/ShowMeshBoundingBox.cc
+++ src/Modules/Visualization/ShowMeshBoundingBox.cc
@@ -1,3 +1,31 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2020 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+
 #include <Core/Algorithms/Visualization/RenderFieldState.h>
 #include <Modules/Visualization/ShowMeshBoundingBox.h>
 #include <Core/Datatypes/Legacy/Field/Field.h>
@@ -121,11 +149,12 @@ ShowMeshBoundingBox::execute()
   auto input = getRequiredInput(InputField);
   if (needToExecute())
   {
+    if (inputsChanged())
+      impl_.setBBox(input->vmesh()->get_bounding_box());
+
     auto state = get_state();
     impl_.setSize(state->getValue(XSize).toInt(), state->getValue(YSize).toInt(),
                   state->getValue(ZSize).toInt());
-    if (inputsChanged())
-      impl_.setBBox(input->vmesh()->get_bounding_box());
 
     sendOutput(OutputGeom, impl_.makeGeomemtry(*this));
   }
