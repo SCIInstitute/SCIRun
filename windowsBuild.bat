a5b1bad0a143d37da86806265f7e4efdae8ee1cd
Dan White <dwhite@sci.utah.edu>
2020-04-17 17:54:50 -0600

    Update windowsBuild.bat

--- windowsBuild.bat
+++ windowsBuild.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set cmake="C:\Program Files\CMake\bin\cmake"
-:: "C:/Qt5.14/5.14.0/msvc2017_64"
+:: "C:\Qt5.14\5.14.0\msvc2017_64"
 set qt=%1
 set cfg=Release
 
@@ -9,7 +9,7 @@ cd bin
 %cmake% ..\Superbuild -DCMAKE_BUILD_TYPE=%cfg% -G"Visual Studio 15 2017" -Ax64 -DQt5_PATH:PATH=%qt%
 %cmake% --build . --config %cfg% -j6
 
-for %%D in (Qt5Core Qt5Gui Qt5Network Qt5OpenGL Qt5PrintSupport Qt5Svg Qt5Widgets) do (copy %qt%\%%D.dll SCIRun\%cfg%\) 
+for %%D in (Qt5Core Qt5Gui Qt5Network Qt5OpenGL Qt5PrintSupport Qt5Svg Qt5Widgets) do (copy %qt%\bin\%%D.dll SCIRun\%cfg%\) 
 copy Externals\Build\Qwt_external\src\%cfg%\qwt.dll SCIRun\%cfg%\
 copy Externals\Source\Python_external\PCbuild\amd64\python38.dll SCIRun\%cfg%\
-xcopy /s %qt%\plugins SCIRun\%cfg%\
+xcopy /s %qt%\plugins\platforms SCIRun\%cfg%\
