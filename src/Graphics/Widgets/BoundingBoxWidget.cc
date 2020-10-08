687861efb3fa9b331d2b9b0ba025231a289dc2c8
Daniel White <dwhite@sci.utah.edu>
2020-10-08 16:47:26 -0600

    Pseudocode from meeting with @RubioJr9

--- src/Graphics/Widgets/BoundingBoxWidget.cc
+++ src/Graphics/Widgets/BoundingBoxWidget.cc
@@ -56,3 +56,93 @@ BasicBoundingBoxWidget::BasicBoundingBoxWidget(const GeneralWidgetParameters& ge
   name_ = gen.glyphMaker->basicBox(params, *this);
   setPosition(params.pos.center_);
 }
+
+// Pseudocode
+#if 0
+BoundingBoxWidget::BoundingBoxWidget(const GeneralWidgetParameters& gen,
+  BasicBoundingBoxParameters params) : CompositeWidget(gen.base)
+{
+  // vertices--sphere widgets
+  // left-click: scale
+  // right-click: axis scale
+  WidgetHandle v0,v1,v2,v3,v4,v5,v6,v7;
+
+  // edges--cylinders
+  // left-click: translate
+  // right-click: axis translate
+  WidgetHandle e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11;
+  // build step:
+  ...
+  .transformMapping({
+    {WidgetInteraction::CLICK, WidgetMovement::TRANSLATE},
+    {WidgetInteraction::RIGHT_CLICK, WidgetMovement::AXIS_TRANSLATE}
+  })
+  ...
+  auto edges = std::vector<WidgetHandle>{e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11};
+
+
+  // faces part 1--disks
+  // left-click: translate/bidirectional scale
+  // right-click: translate/unidirectional scale
+  WidgetHandle fd0, fd1, fd2, fd3, fd4, fd5;
+
+
+
+  //TODO:
+  // face spheres
+  WidgetHandle fs0, fs1, fs2, fs3, fs4, fs5;
+
+
+
+  for (auto& e : edges)
+  {
+    registerAllSiblingWidgetsForEvent(e, WidgetMovement::TRANSLATE);
+  }
+
+  // face 0 = {v0,v1,v2,v3}
+  // axis translate
+  fd0 translates along an axis--T0
+  all v from face 0, {v0,v1,v2,v3}, also translate--T0
+  all e from face 0, {e0,e1,e2,e3} also translate--T0
+  opposite face {v4,v5,v6,v7},{e4,e5,e6,e7},{fd5,fs5}, is fixed--Id
+  parallel edges--{e8,e9,e10,e11}--uni-scale/translate along axis--T1
+  anything else on parallel faces {fs/fd...} --T0/2
+
+  void wireTheAboveMovementScheme(clicked, translated, fixed?, uniscaled+translated, halfTranslated)
+  {
+    clicked << propagatesEvent<WidgetMovement::TRANSLATE>::to << TheseWidgets {translated};
+    clicked << propagatesEvent<WidgetMovement::UNISCALED_TRANSLATED>::to << uniscaled+translated;
+    clicked << propagatesEvent<WidgetMovement::MODIFIED_TRANSLATE>::to << halfTranslated;
+  }
+
+  wireTheAboveMovementScheme(fd0, translated={v0,v1,v2,v3,e0,e1,e2,e3},
+    ? id/left alone={v4,v5,v6,v7},{e4,e5,e6,e7},{fd5,fs5}, //depends on testability
+    uniscaledParallelEdges = {e8,e9,e10,e11}
+  );
+  //can we use mesh geometry functions to get all the relative components functionally?
+
+  wireTheAboveMovementScheme(fd1, translated={v0,v1,v2,v3,e0,e1,e2,e3},
+    ? id/left alone={v4,v5,v6,v7},{e4,e5,e6,e7},{fd5,fs5}, //depends on testability
+    uniscaledParallelEdges = {e8,e9,e10,e11}
+  );
+  wireTheAboveMovementScheme(clicked, translated={v0,v1,v2,v3,e0,e1,e2,e3},
+    ? id/left alone={v4,v5,v6,v7},{e4,e5,e6,e7},{fd5,fs5}, //depends on testability
+    uniscaledParallelEdges = {e8,e9,e10,e11}
+  );
+  wireTheAboveMovementScheme(clicked, translated={v0,v1,v2,v3,e0,e1,e2,e3},
+    ? id/left alone={v4,v5,v6,v7},{e4,e5,e6,e7},{fd5,fs5}, //depends on testability
+    uniscaledParallelEdges = {e8,e9,e10,e11}
+  );
+  wireTheAboveMovementScheme(clicked, translated={v0,v1,v2,v3,e0,e1,e2,e3},
+    ? id/left alone={v4,v5,v6,v7},{e4,e5,e6,e7},{fd5,fs5}, //depends on testability
+    uniscaledParallelEdges = {e8,e9,e10,e11}
+  );
+  wireTheAboveMovementScheme(clicked, translated={v0,v1,v2,v3,e0,e1,e2,e3},
+    ? id/left alone={v4,v5,v6,v7},{e4,e5,e6,e7},{fd5,fs5}, //depends on testability
+    uniscaledParallelEdges = {e8,e9,e10,e11}
+  );
+
+
+
+}
+#endif
