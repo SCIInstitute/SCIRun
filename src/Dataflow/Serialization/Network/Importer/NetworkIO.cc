6ef60cf59570e11a4449e5a1ca95f8bcc4885322
Nidhi Patel <nids2001@gmail.com>
2021-01-20 20:32:09 -0500

    Rename CalculateNodeNorms to GenerateNodeNorms in rename map

--- src/Dataflow/Serialization/Network/Importer/NetworkIO.cc
+++ src/Dataflow/Serialization/Network/Importer/NetworkIO.cc
@@ -224,7 +224,7 @@ const std::map<std::string, std::string> LegacyNetworkIO::moduleRenameMap_ =
     { "BuildElemLeadField", "PlaceholderModule" },
     { "OptimizeDipole", "PlaceholderModule" },
     { "OptimizeConductivities", "PlaceholderModule" },
-    { "CalculateNodeNormals", "PlaceholderModule" },
+    { "CalculateNodeNormals", "GenerateNodeNormals" },
     { "SynchronizeGeometry", "PlaceholderModule" },
     { "TransformData", "PlaceholderModule" },
     { "SetFieldOrMeshStringProperty", "PlaceholderModule" },
@@ -1208,7 +1208,7 @@ LegacyNetworkIO::load_net(const std::string &net)
   net_file_ = net;
   if (!load_network())
   {
-    logCritical("!!!!!!! Network import unsuccessful: {}", net);  
+    logCritical("!!!!!!! Network import unsuccessful: {}", net);
 
     return nullptr;
   }
