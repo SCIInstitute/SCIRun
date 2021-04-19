76367f0d150e6c1b7101198046cbd0cbb718e276
Daniel White <dwhite@sci.utah.edu>
2021-04-19 13:41:58 -0600

    Big batch of override warnings fixed

--- src/Core/Algorithms/Legacy/Fields/MarchingCubes/MarchingCubes.cc
+++ src/Core/Algorithms/Legacy/Fields/MarchingCubes/MarchingCubes.cc
@@ -78,9 +78,8 @@ AlgorithmParameterName MarchingCubesAlgo::build_node_interpolant("build_node_int
 AlgorithmParameterName MarchingCubesAlgo::build_elem_interpolant("build_elem_interpolant");
 AlgorithmParameterName MarchingCubesAlgo::num_threads("num_threads");
 
-AlgorithmOutput MarchingCubesAlgo::run(const AlgorithmInput& input) const
+AlgorithmOutput MarchingCubesAlgo::run(const AlgorithmInput&) const
 {
-
   AlgorithmOutput output;
   return output;
 }
