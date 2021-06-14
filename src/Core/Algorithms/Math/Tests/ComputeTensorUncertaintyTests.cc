ce2695029ff9cf6b4910f8ea92d3a2833238eefb
RubioJr9 <u0893472@utah.edu>
2021-06-14 17:12:17 -0600

    Added tests for log euclidean and linear invariant

--- src/Core/Algorithms/Math/Tests/ComputeTensorUncertaintyTests.cc
+++ src/Core/Algorithms/Math/Tests/ComputeTensorUncertaintyTests.cc
@@ -49,40 +49,80 @@ using namespace SCIRun::Core::Algorithms;
 using namespace SCIRun::Core::Algorithms::Math;
 
 const auto DIM = 3;
-const auto epsilon = 1.0e-12;
 const std::set<std::string> options = {"Linear Invariant","Log-Euclidean","Matrix Average"};
 const auto MATRIX_AVERAGE = AlgoOption("Matrix Average", options);
 const auto LOG_EUCLIDEAN = AlgoOption("Log-Euclidean", options);
 const auto LINEAR_INVARIANT = AlgoOption("Linear Invariant", options);
 
-// Used test from ReportFieldInfoTests as template for setting tensor data
-TEST(ComputeTensorUncertaintTest, SetFieldData)
+FieldHandle tensorToField(Tensor t)
+{
+  FieldInformation lfi("LatVolMesh", 1, "Tensor");
+  MeshHandle mesh = CreateMesh(lfi);
+  FieldHandle fh = CreateField(lfi, mesh);
+  fh->vfield()->set_all_values(t);
+  return fh;
+}
+
+void assertTensorsNear(const Tensor& t, const Tensor& expected, double epsilon)
+{
+  for (int i = 0; i < DIM; ++i)
+    for (int j = 0; j < DIM; ++j)
+      ASSERT_NEAR(t.val(i, j), expected.val(i, j), epsilon);
+}
+
+TEST(ComputeTensorUncertaintyTest, MatrixAverage)
 {
   const double unitHalf = 0.5 * std::sqrt(2);
-  Tensor t1(5*Vector(1,0,0), 3*Vector(0,1,0), Vector(0,0,1));
-  Tensor t2(7*Vector(unitHalf,unitHalf,0), 2*Vector(unitHalf,-unitHalf,0), Vector(0,0,1));
+  auto fh1 = tensorToField(Tensor(5*Vector(1,0,0), 3*Vector(0,1,0), Vector(0,0,1)));
+  auto fh2 = tensorToField(Tensor(7*Vector(unitHalf,unitHalf,0), 2*Vector(unitHalf,-unitHalf,0), Vector(0,0,1)));
   Tensor t_expected(4.75, 1.25, 0, 3.75, 0, 1);
-  FieldInformation lfi("LatVolMesh", 1, "Tensor");
-
-  MeshHandle mesh1 = CreateMesh(lfi);
-  MeshHandle mesh2 = CreateMesh(lfi);
-  FieldHandle fh1 = CreateField(lfi, mesh1);
-  FieldHandle fh2 = CreateField(lfi, mesh2);
-  fh1->vfield()->set_all_values(t1);
-  fh2->vfield()->set_all_values(t2);
-
-  Tensor t;
-  fh1->vfield()->get_value(t, 0);
-  std::cout << "t1 from field: " << t << std::endl;
-  fh2->vfield()->get_value(t, 0);
-  std::cout << "t2 from field: " << t << std::endl;
+  const auto epsilon = 1.0e-12;
 
   FieldList fields = {fh1, fh2};
   ComputeTensorUncertaintyAlgorithm algo;
   boost::tuple<FieldHandle, MatrixHandle> output = algo.runImpl(fields, MATRIX_AVERAGE, MATRIX_AVERAGE);
+
+  Tensor t;
   output.get<0>()->vfield()->get_value(t,0);
-  std::cout << "t average: " << t << std::endl;
-  for (int i = 0; i < DIM; ++i)
-    for (int j = 0; j < DIM; ++j)
-      ASSERT_NEAR(t.val(i, j), t_expected.val(i, j), epsilon);
+  assertTensorsNear(t, t_expected, epsilon);
+}
+
+// I hand calculated the Log-Euclidean calcuation except the last step of matrix exponential, hence the large epsilon
+TEST(ComputeTensorUncertaintyTest, LogEuclidean)
+{
+  const double unitHalf = 0.5 * std::sqrt(2);
+  auto fh1 = tensorToField(Tensor(5*Vector(1,0,0), 3*Vector(0,1,0), Vector(0,0,1)));
+  auto fh2 = tensorToField(Tensor(7*Vector(unitHalf,unitHalf,0), 2*Vector(unitHalf,-unitHalf,0), Vector(0,0,1)));
+  Tensor t_expected(4.522, 1.215, 0, 3.531, 0, 1);
+  const auto epsilon = 0.001;
+
+  FieldList fields = {fh1, fh2};
+  ComputeTensorUncertaintyAlgorithm algo;
+  boost::tuple<FieldHandle, MatrixHandle> output = algo.runImpl(fields, LOG_EUCLIDEAN, LOG_EUCLIDEAN);
+
+  Tensor t;
+  output.get<0>()->vfield()->get_value(t,0);
+  assertTensorsNear(t, t_expected, epsilon);
+}
+
+// Since this only tests the invariant calculation, we only need to check the eigenvalues
+TEST(ComputeTensorUncertaintyTest, LinearInvariant)
+{
+  const double unitHalf = 0.5 * std::sqrt(2);
+  auto fh1 = tensorToField(Tensor(5*Vector(1,0,0), 3*Vector(0,1,0), Vector(0,0,1)));
+  auto fh2 = tensorToField(Tensor(7*Vector(unitHalf,unitHalf,0), 2*Vector(unitHalf,-unitHalf,0), Vector(0,0,1)));
+  std::vector<double> eigs_expected = {5.9075, 2.715, 0.877};
+  const auto epsilon = 0.003;
+
+  FieldList fields = {fh1, fh2};
+  ComputeTensorUncertaintyAlgorithm algo;
+  boost::tuple<FieldHandle, MatrixHandle> output = algo.runImpl(fields, LINEAR_INVARIANT, MATRIX_AVERAGE);
+
+  Tensor t;
+  output.get<0>()->vfield()->get_value(t,0);
+  std::vector<double> eigs(3);
+  t.get_eigenvalues(eigs[0], eigs[1], eigs[2]);
+
+  for (auto i = 0; i < DIM; ++i)
+    ASSERT_NEAR(eigs[i], eigs_expected[i], epsilon);
 }
