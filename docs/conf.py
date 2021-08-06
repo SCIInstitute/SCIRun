543d44aa637f36c8be44f42b28d20f937f8dea8a
Nidhi P <nids2001@gmail.com>
2021-08-06 15:23:13 -0400

    Move to MyST and update theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-import recommonmark
-from recommonmark.transform import AutoStructify
-
 # -- Project information -----------------------------------------------------
 
 project = 'SCIRun'
@@ -32,13 +29,36 @@ release = '0.01'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+# extensions = [
+#         'sphinx.ext.autodoc',
+#         'sphinx.ext.napoleon',
+#         'recommonmark',
+#         'sphinxcontrib.bibtex',
+#         'sphinx_markdown_tables'
+# ]
+
 extensions = [
         'sphinx.ext.autodoc',
         'sphinx.ext.napoleon',
-        'recommonmark',
+        'myst_parser',
+        'sphinx_markdown_tables',
         'sphinxcontrib.bibtex',
-        'sphinx_markdown_tables'
+        'notfound.extension'
 ]
+
+myst_enable_extensions = [
+    "colon_fence",  # Allow code fence using ::: (see https://myst-parser.readthedocs.io/en/latest/using/syntax-optional.html#syntax-colon-fence)
+    "linkify",  # Allow automatic creation of links from URLs (it is sufficient to write https://google.com instead of <https://google.com>)
+    "substitution"
+]
+
+# Auto-generate header anchors up to level 6, so that it can be referenced like [](file.md#header-anchor).
+# (see https://myst-parser.readthedocs.io/en/latest/using/syntax-optional.html#auto-generated-header-anchors)
+myst_heading_anchors = 6
+
+# auto number figures
+numfig = True
 
 # Path for bibtex files
 bibtex_bibfiles = ['references.bib']
@@ -56,6 +76,18 @@ source_suffix = {
     '.md': 'markdown'
 }
 
+notfound_context = {
+    'title': 'Page Not Found',
+    'body': '''
+<h1>Page Not Found</h1>
+<p>Sorry, we couldn't find that page.</p>
+<p>Try using the search box or go to the homepage.</p>
+''',
+}
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
 # the master toctree doc
 master_doc = 'index'
 
@@ -71,18 +103,20 @@ language = 'python'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme = 'sphinx_book_theme'
+
 # html_theme = 'alabaster'
 
 #html_theme = 'groundwork'
 
 # html_style = '/css/main.css'
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#
+# if not on_rtd:  # only import and set the theme if we're building docs locally
+#     import sphinx_rtd_theme
+#     html_theme = 'sphinx_rtd_theme'
+#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -102,13 +136,3 @@ html_theme_options = {
 }
 
 autosectionlabel_prefix_document = True
-
-def setup(app):
-     app.add_config_value('recommonmark_config', {
-         #'url_resolver': lambda url: github_doc_root + url,
-         'auto_toc_tree_section': 'Contents',
-         'enable_math': False,
-         'enable_inline_math': False,
-         'enable_eval_rst': True,
-     }, True)
-     app.add_transform(AutoStructify)
