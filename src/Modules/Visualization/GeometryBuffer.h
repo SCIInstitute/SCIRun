0f9cc49a391852c521f32014c201cecaf15d8987
Daniel White <dwhite@sci.utah.edu>
2021-10-22 17:32:58 -0600

    Add module skeleton

---
+++ src/Modules/Visualization/GeometryBuffer.h
@@ -0,0 +1,66 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2020 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+
+
+#ifndef MODULES_VISUALIZATION_GEOMETRY_BUFFER_H
+#define MODULES_VISUALIZATION_GEOMETRY_BUFFER_H
+
+#include <Dataflow/Network/ModuleWithAsyncDynamicPorts.h>
+#include <Modules/Visualization/share.h>
+
+namespace SCIRun {
+
+  namespace Core {
+    namespace Algorithms {
+      namespace Visualization {
+
+        ALGORITHM_PARAMETER_DECL(BufferSize);
+        ALGORITHM_PARAMETER_DECL(FrameDelay);
+
+      }
+    }
+  }
+
+namespace Modules {
+namespace Visualization {
+
+  class SCISHARE GeometryBuffer : public SCIRun::Dataflow::Networks::ModuleWithAsyncDynamicPorts,
+    public Has1InputPort<AsyncDynamicPortTag<GeometryPortTag>>,
+    public Has1OutputPort<GeometryPortTag>
+  {
+  public:
+    GeometryBuffer();
+    void execute() override;
+    void asyncExecute(const Dataflow::Networks::PortId& pid, Core::Datatypes::DatatypeHandle data) override;
+    void setStateDefaults() override;
+    INPUT_PORT(0, GeometryInput, GeometryObject);
+    OUTPUT_PORT(0, GeometryOutputSeries, GeometryObject);
+    MODULE_TRAITS_AND_INFO(ModuleFlags::ModuleHasUI)
+  };
+}}}
+
+#endif
