c955ef45706fd2b4c0f4dc16efc9b01a3eddc2de
jessdtate <jess@sci.utah.edu>
2021-12-09 11:25:41 -0700

    merging @kimjohn1 changes to fresh branch

---
+++ src/Core/Algorithms/ParticleInCell/GravitySimAlgo.h
@@ -0,0 +1,33 @@
+
+#ifndef CORE_ALGORITHMS_ParticleInCell_GravitySimAlgo_H
+#define CORE_ALGORITHMS_ParticleInCell_GravitySimAlgo_H
+
+#include <Core/Datatypes/Matrix.h>
+#include <Core/Datatypes/DenseMatrix.h>
+#include <Core/Datatypes/DenseColumnMatrix.h>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+#include <Core/Algorithms/Base/AlgorithmVariableNames.h>
+#include <Core/Algorithms/Base/AlgorithmBase.h>
+#include <Core/Algorithms/ParticleInCell/share.h>
+
+namespace SCIRun {
+namespace Core {
+namespace Algorithms {
+namespace ParticleInCell {
+
+class SCISHARE GravitySimAlgo : public AlgorithmBase
+    {
+/*
+
+    public:
+      GravitySimAlgo();
+*/
+    };
+
+}}}}
+#endif
