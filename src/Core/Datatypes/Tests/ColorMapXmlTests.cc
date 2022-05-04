5d20b82bd040dd3cb304231c1e954e4e86747e2b
Dan White <dwhite@sci.utah.edu>
2022-05-04 12:48:17 -0600

    Add pugixml

--- src/Core/Datatypes/Tests/ColorMapXmlTests.cc
+++ src/Core/Datatypes/Tests/ColorMapXmlTests.cc
@@ -32,6 +32,7 @@
 #include <Core/GeometryPrimitives/Tensor.h>
 #include <Core/GeometryPrimitives/Vector.h>
 #include <Core/GeometryPrimitives/Point.h>
+#include <pugixml/pugixml.hpp>
 
 using ::testing::_;
 using ::testing::NiceMock;
@@ -55,14 +56,90 @@ namespace ColorXml
   {
     std::vector<ColorMap> maps;
   };
+
+  ColorMaps readColorMapXml(const std::string& filename)
+  {
+    pugi::xml_document doc;
+    auto result = doc.load_file(filename.c_str());
+    if (!result)
+      return {};
+
+    ColorMaps colorMaps;
+    for (const auto& cm : doc.child("ColorMaps"))
+    {
+      ColorMap colorMap;
+      colorMap.name = cm.attribute("name").as_string();
+      colorMap.space = cm.attribute("space").as_string();
+      colorMaps.maps.push_back(colorMap);
+    }
+    return colorMaps;
+  }
 }
+
 
 TEST(ColorMapXmlTests, CanReadColorMapXmlFromParaview)
 {
-  FAIL() << "TODO";
-  // ColorRGB c(1.0,2.0,3.0);
-  // const std::string expected = "Color(1,2,3)";
-  // EXPECT_EQ(expected, c.toString());
-  // ColorRGB c2(expected);
-  // EXPECT_EQ(c, c2);
+  pugi::xml_document doc;
+  auto result = doc.load_file("/Users/dan/Downloads/colormaps/All_idl_cmaps.xml");
+  if (!result)
+    FAIL() << "Couldn't read xml file";
+
+  std::vector<std::string> colorMapNames;
+  for (const auto& cm : doc.child("ColorMaps"))
+  {
+    auto name = cm.attribute("name").as_string();
+    //std::cout << "\"" << name << "\",\n";
+    colorMapNames.push_back(name);
+  }
+  std::vector<std::string> expectedColorMapNames =
+  {
+    "B-W_LINEAR",
+    "BLUE-WHITE",
+    "GRN-RED-BLU-WHT",
+    "RED_TEMPERATURE",
+    "BLUE-GREEN-RED-YELLOW",
+    "STD_GAMMA-II",
+    "PRISM",
+    "RED-PURPLE",
+    "GREEN-WHITE_LINEAR",
+    "GRN-WHT_EXPONENTIAL",
+    "GREEN-PINK",
+    "BLUE-RED",
+    "16_LEVEL",
+    "RAINBOW",
+    "STEPS",
+    "STERN_SPECIAL",
+    "Haze",
+    "Blue_-_Pastel_-_Red",
+    "Pastels",
+    "Hue_Sat_Lightness_1",
+    "Hue_Sat_Lightness_2",
+    "Hue_Sat_Value_1",
+    "Hue_Sat_Value_2",
+    "Purple-Red_Stripes",
+    "Beach",
+    "Mac_Style",
+    "Eos_A",
+    "Eos_B",
+    "Hardcandy",
+    "Nature",
+    "Ocean",
+    "Peppermint",
+    "Plasma",
+    "Blue-Red",
+    "Rainbow",
+    "Blue_Waves",
+    "Volcano",
+    "Waves",
+    "Rainbow18",
+    "Rainbow_white",
+    "Rainbow_black"
+  };
+  EXPECT_EQ(colorMapNames, expectedColorMapNames);
+}
+
+TEST(ColorMapXmlTests, CanCreateColorMapXmlData)
+{
+  auto colorMaps = ColorXml::readColorMapXml("/Users/dan/Downloads/colormaps/All_idl_cmaps.xml");
+  EXPECT_EQ(colorMaps.maps.size(), 41);
 }
