ea0b3593cddbcf6ac7fbe7a28ea0f23b4302a5fb
kimjohn1 <kimjohnpeterson@gmail.com>
2022-08-10 07:37:21 -0600

    scalar field reader in PIConGPUReader is active, not tested

--- src/Modules/ParticleInCell/PIConGPUReader.cc
+++ src/Modules/ParticleInCell/PIConGPUReader.cc
@@ -28,13 +28,14 @@
 #include <openPMD/openPMD.hpp>
 #include <filesystem>
 
+#include <Core/Datatypes/MatrixTypeConversions.h>
 #include <Modules/ParticleInCell/PIConGPUReader.h>
 #include <Core/Algorithms/ParticleInCell/PIConGPUReaderAlgo.h>
-#include <Core/Datatypes/MatrixTypeConversions.h>
 
 using namespace SCIRun;
 using namespace SCIRun::Core::Datatypes;
 using namespace SCIRun::Core::Algorithms;
+using namespace SCIRun::Dataflow::Networks; 
 using namespace SCIRun::Modules::ParticleInCell;
 using namespace SCIRun::Core::Algorithms::ParticleInCell;
 
@@ -152,22 +153,11 @@ void PIConGPUReader::execute()
                                                         //Wait for simulation output data to be generated and posted via SST
                                                         // TODO: figure out how to use a general reference for the home directory in these two lines of code
 
-//        std::filesystem::path SST_dir = '$HOME/scratch/runs/SST/simOutput/openPMD/simData.sst';
-//        std::string home = "~";
-//        std::string home = "/home/kj";
-//        std::string home = "../..";
         std::string SST_dir = "../../../scratch/runs/SST/simOutput/openPMD/simData.sst";
 
-        char pwd[PATH_MAX];
-        getcwd(pwd,sizeof(pwd));
-        cout << "\nDebug: present working directory is " << pwd <<"\n";
-
-//        while(!std::filesystem::exists("/home/kj/scratch/runs/SST/simOutput/openPMD/simData.sst")) sleep(1);
-//        while(!std::filesystem::exists("~/scratch/runs/SST/simOutput/openPMD/simData.sst")) sleep(1);
         while(!std::filesystem::exists(SST_dir)) sleep(1);
 
         Series series = Series("/home/kj/scratch/runs/SST/simOutput/openPMD/simData.sst", Access::READ_ONLY);
-//        Series series = Series("scratch/runs/SST/simOutput/openPMD/simData.sst", Access::READ_ONLY);
 
         for (IndexedIteration iteration : series.readIterations())
             {
@@ -175,95 +165,112 @@ void PIConGPUReader::execute()
 
 /**/
 // New code  Use the input variables to determine what data to load and which output function to call
-            std::string spec = "e";                                         //set development input variables
-            std::string particle_feature = "position";
+            std::string spec = "e";                                  //set development input variables
             std::string output_option    = "particles";
             int particle_sample_rate     = 100;
 
-            if(output_option.compare("particles")==0) //output_option is an input data.
+                                                                     //Read particle data
+            Record particlePositions = iteration.particles[spec]["position"];
+
+            std::array<std::shared_ptr<position_t>, 3> loadedChunks;
+            std::array<Extent, 3> extents;
+            std::array<std::string, 3> const dimensions{{"x", "y", "z"}};
+
+            for (size_t i_dim = 0; i_dim < 3; ++i_dim)
                 {
-                Record particlePositions = iteration.particles[spec]["position"];
+                std::string dim_str = dimensions[i_dim];
+                RecordComponent rc = particlePositions[dim_str];
+                loadedChunks[i_dim] = rc.loadChunk<position_t>(Offset(rc.getDimensionality(), 0), rc.getExtent());
+                extents[i_dim] = rc.getExtent();
+                }
 
-                std::array<std::shared_ptr<position_t>, 3> loadedChunks;
-                std::array<Extent, 3> extents;
-                std::array<std::string, 3> const dimensions{{"x", "y", "z"}};
+            iteration.seriesFlush();                                 //Data is now available
 
-                for (size_t i_dim = 0; i_dim < 3; ++i_dim)
-                    {
-                    std::string dim_str = dimensions[i_dim];
-                    RecordComponent rc = particlePositions[dim_str];
-                    loadedChunks[i_dim] = rc.loadChunk<position_t>(Offset(rc.getDimensionality(), 0), rc.getExtent());
-                    extents[i_dim] = rc.getExtent();
-                    }
-
-                iteration.seriesFlush();                    //Data is now available
-
-
-                Extent const &extent_0 = extents[0];
-                int num_particles = extent_0[0];
+            Extent const &extent_0 = extents[0];
+            int num_particles = extent_0[0];
 //                const int one_Dim = 3*num_particles;
 
-                const int buffer_size  = 1+(num_particles/particle_sample_rate);
-                auto component_x       = new float[buffer_size];
-                auto component_y       = new float[buffer_size];
-                auto component_z       = new float[buffer_size];
+            const int buffer_size  = 1+(num_particles/particle_sample_rate);
+            auto component_x       = new float[buffer_size];
+            auto component_y       = new float[buffer_size];
+            auto component_z       = new float[buffer_size];
 
-//                iteration.close();
-                                                            //Call the output function
+                                                        //Call the output function
 //                particleData(buffer_size, component_x, component_y, component_z);
 
 //    *****************************************************  The output function code is moved here for now
-//    *****************************************************  Set up, load and send the data to be output
+//    *****************************************************  Load the data to be output
 
-                auto flat_particle_feature = new float[buffer_size*3];                            
-                for (int k = 0; k<buffer_size; k++)
-                    {
-                    flat_particle_feature[k]              =component_x[k];
-                    flat_particle_feature[k+buffer_size]  =component_y[k];
-                    flat_particle_feature[k+2*buffer_size]=component_z[k];
-                    }
-
-            //    *****************************************************  Set up the data to be output
-                DenseMatrixHandle output_mat_0(new DenseMatrix(buffer_size*3, 1));
-
-                double *data0=output_mat_0->data();
-
-                std::copy(flat_particle_feature, flat_particle_feature+buffer_size*3, data0);
-
-            //    *****************************************************  Send data to the output port
-                sendOutput(x_coordinates, output_mat_0);
+            auto flat_particle_feature = new float[buffer_size*3];                            
+            for (int k = 0; k<buffer_size; k++)
+                {
+                flat_particle_feature[k]              =component_x[k];
+                flat_particle_feature[k+buffer_size]  =component_y[k];
+                flat_particle_feature[k+2*buffer_size]=component_z[k];
                 }
+
+        //    *****************************************************  Set up the output data structure
+            DenseMatrixHandle output_mat_0(new DenseMatrix(buffer_size*3, 1));
+            double *data0=output_mat_0->data();
+            std::copy(flat_particle_feature, flat_particle_feature+buffer_size*3, data0);
+
+        //    *****************************************************  Send data to the output port
+            sendOutput(x_coordinates, output_mat_0);
+                                                                     //End of Particle data processing
+
+
+/**/
+                                                                     //Read Scalar field data (ijk values at xyz node points is from Franz Poschel email, 17 May 2022)
+
+            std::string scalar_field_component = "e_all_chargeDensity";
+
+            auto scalarFieldData               = iteration.meshes[scalar_field_component][MeshRecordComponent::SCALAR];
+            auto scalarFieldData_buffer        = scalarFieldData.loadChunk<float>();
+            iteration.seriesFlush();                    //Data is now available
+
+                                                        //Call the output function
+    //            scalarField(mesh, component_x);
+
+//    *****************************************************  The output function code is moved here for now
+//    *****************************************************  Load the data to be output
+
+            auto extent_sFD           = scalarFieldData.getExtent();
+            const int buffer_size_sFD = extent_sFD[0] * extent_sFD[1] * extent_sFD[2];
+            auto buffer_sFD           = new double[buffer_size_sFD];
+
+            for (size_t i = 0; i < extent_sFD[0]; ++i) for (size_t j = 0; j < extent_sFD[1]; ++j) for (size_t k = 0; k < extent_sFD[2]; ++k)
+                {
+                size_t flat_index = i * extent_sFD[1] * extent_sFD[2] + j * extent_sFD[2] + k;
+                buffer_sFD[flat_index] = scalarFieldData_buffer.get()[flat_index];
+                }
+
+//    *****************************************************  Set up the output data structure
+
+            DenseMatrixHandle output_mat_1(new DenseMatrix(buffer_size_sFD, 1));
+            double *data1=output_mat_1->data();
+            std::copy(buffer_sFD, buffer_sFD+buffer_size_sFD, data1);
+
+//    *****************************************************  Send data to the output ports
+            sendOutput(y_coordinates, output_mat_1);
+                                                                     //End of Scalar Field data processing
+
+
 
 /*
+                                                        //Prototype vector field output (ijk values at xyz node points is from Franz Poschel email, 17 May 2022)
+    //        if(output_option=="vector_field")
+    //            {
+            auto mesh = iteration.meshes["E"];
+            auto component_x = mesh["x"].loadChunk<float>();
+            auto component_y = mesh["y"].loadChunk<float>();
+            auto component_z = mesh["z"].loadChunk<float>();
+            iteration.seriesFlush();                    //Data is now available
 
-                                                        //Prototype scalar field output function call (ijk values at xyz node points is from Franz Poschel email, 17 May 2022)
-            if(output_option=="scalar_field")
-                {
-                auto mesh = iteration.meshes["E"];
-                auto component_x = mesh["x"].loadChunk<float>();
-                iteration.seriesFlush();                    //Data is now available
-
-                                                            //Call the output function
-                vectorField(mesh, component_x);
-                }
+                                                        //Call the output function
+    //            vectorField(mesh, component_x, component_y, component_z);
+    //            }
 */
-
-
-/*
-                                                        //Prototype vector field output function call (ijk values at xyz node points is from Franz Poschel email, 17 May 2022)
-            if(output_option=="vector_field")
-                {
-                auto mesh = iteration.meshes["E"];
-                auto component_x = mesh["x"].loadChunk<float>();
-                auto component_y = mesh["y"].loadChunk<float>();
-                auto component_z = mesh["z"].loadChunk<float>();
-                iteration.seriesFlush();                    //Data is now available
-
-                                                            //Call the output function
-                vectorField(mesh, component_x, component_y, component_z);
-                }
-*/
-                iteration.close();
+            iteration.close();
 
 
 
