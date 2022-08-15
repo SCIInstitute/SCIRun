80f822c190cc2d0d5159ad8217964214ea0d95ad
kimjohn1 <kimjohnpeterson@gmail.com>
2022-08-15 12:35:41 -0600

    PIConGPUReader ParticleData still fails to compile

--- src/Modules/ParticleInCell/PIConGPUReader.cc
+++ src/Modules/ParticleInCell/PIConGPUReader.cc
@@ -28,19 +28,21 @@
 #include <openPMD/openPMD.hpp>
 #include <filesystem>
 
+//#include <Core/Datatypes/DenseMatrix.h>
+//#include <Core/Algorithms/Legacy/Fields/FieldData/SetFieldData.h>
+//#include <Core/Algorithms/Base/AlgorithmPreconditions.h>
+//#include <Core/Algorithms/Base/AlgorithmVariableNames.h>
+
 #include <Core/Datatypes/MatrixTypeConversions.h>
 #include <Modules/ParticleInCell/PIConGPUReader.h>
 #include <Core/Algorithms/ParticleInCell/PIConGPUReaderAlgo.h>
-#include <Core/GeometryPrimitives/Vector.h>
 
+#include <Core/GeometryPrimitives/Vector.h>
 #include <Core/GeometryPrimitives/Point.h>
 #include <Core/Datatypes/Legacy/Field/Field.h>
 #include <Core/Datatypes/Legacy/Field/VField.h>
 #include <Core/Datatypes/Legacy/Field/VMesh.h>
 #include <Core/Datatypes/Legacy/Field/FieldInformation.h>
-
-#include <Modules/Fields/CalculateNodeLocationFrequency.h>
-#include <Core/Datatypes/Legacy/Field/Field.h>
 
 using namespace SCIRun;
 using namespace SCIRun::Core::Datatypes;
@@ -49,7 +51,6 @@ using namespace SCIRun::Dataflow::Networks;
 using namespace SCIRun::Modules::ParticleInCell;
 using namespace SCIRun::Core::Algorithms::ParticleInCell;
 using namespace SCIRun::Core::Geometry;
-using namespace SCIRun::Modules::Fields;
 
 using std::cout;
 using namespace openPMD;
@@ -78,8 +79,8 @@ FieldHandle PIConGPUReader::particleData(int buffer_size, float component_x[], f
     FieldInformation pcfi("PointCloudMesh",0,"int");
     MeshHandle mesh = CreateMesh(pcfi);
 
-//    mesh->reserve_nodes(buffer_size);                              //this variable produces a compile error, I have temporarily made it a comment
-                                                                     //the error is: error: ‘using element_type = class SCIRun::Mesh’ {aka ‘class SCIRun::Mesh’} has no member named ‘reserve_nodes’
+//    mesh->reserve_nodes(buffer_size);                              //this variable produces a compile error, I have temporarily made it a comment, the error is: 
+                                                                     //error: ‘using element_type = class SCIRun::Mesh’ {aka ‘class SCIRun::Mesh’} has no member named ‘reserve_nodes’
     
     FieldHandle field = CreateField(pcfi,mesh);
 
@@ -95,8 +96,10 @@ FieldHandle PIConGPUReader::particleData(int buffer_size, float component_x[], f
     return field;
     }
 
-FieldHandle PIConGPUReader::scalarField(const int buffer_size_sFD, std::shared_ptr<float> scalarFieldData_buffer, std::vector<long unsigned int> extent_sFD)
+FieldHandle PIConGPUReader::scalarField(const int numvals, std::shared_ptr<float> scalarFieldData_buffer, std::vector<long unsigned int> extent_sFD)
     {
+/*Jess original code:
+
     FieldInformation lfi("LatVolMesh",1,"double");
     MeshHandle mesh = CreateMesh(lfi,extent_sFD[0], extent_sFD[1], extent_sFD[2], Point(0.0,0.0,0.0), Point(1.0,1.0,1.0));
     FieldHandle ofh = CreateField(lfi,mesh);
@@ -104,43 +107,28 @@ FieldHandle PIConGPUReader::scalarField(const int buffer_size_sFD, std::shared_p
     VMesh* omesh = ofh->vmesh();                                     //This variable is noted as not being used - possibly not needed??
     VField* ofield = ofh->vfield();
 
-//    ofield->set_values(scalarFieldData_buffer);                    //This step causes a compile error, appears to require a data type of const T* and may need a secong argument, I have temporarily made it a comment
-                                                                     //the error is: error: no matching function for call to ‘SCIRun::VField::set_values(std::shared_ptr<float>&)’
-    
-    return ofh;
-/*
-                                                                     //The original code for saving a vector field follows
-            std::string vector_field_type = "E";
-            auto vectorFieldData          = iteration.meshes[vector_field_type];
-            auto vFD_component_x          = vectorFieldData["x"].loadChunk<float>();
-            auto vFD_component_y          = vectorFieldData["y"].loadChunk<float>();
-            auto vFD_component_z          = vectorFieldData["z"].loadChunk<float>();
-            iteration.seriesFlush();                    //Data is now available
-
-            auto extent_vFD   = vectorFieldData["x"].getExtent();
-            const int one_Dim = 3*(extent_vFD[0] * extent_vFD[1] * extent_vFD[2]);
-            auto XYZ_vec      = new double[one_Dim];
-
-//    *****************************************************  The output function
-//    *****************************************************  Load the data to be output
-
-            for(size_t i = 0; i < extent_vFD[0]; ++i) for(size_t j = 0; j < extent_vFD[1]; ++j) for(size_t k = 0; k < extent_vFD[2]; ++k)
-                {
-                size_t flat_index                                                     = i * extent_vFD[1] * extent_vFD[2] + j * extent_vFD[2] + k;
-                XYZ_vec[flat_index]                                                   = vFD_component_x.get()[flat_index];
-                XYZ_vec[flat_index + (extent_vFD[0]*extent_vFD[1]*extent_vFD[2])]     = vFD_component_y.get()[flat_index];
-                XYZ_vec[flat_index + (extent_vFD[0]*extent_vFD[1]*extent_vFD[2]) * 2] = vFD_component_z.get()[flat_index];
-                }
-
-//    *****************************************************  Set up the output data structure
-            DenseMatrixHandle output_mat_2(new DenseMatrix(one_Dim, 1));
-            double *data2=output_mat_2->data();
-            std::copy(XYZ_vec, XYZ_vec+one_Dim, data2);
-
-//    *****************************************************  Send data to the output port
-            sendOutput(z_coordinates, output_mat_2);
-            sendOutput(XYZ_vec, output_mat_2);
+    ofield->set_values(scalarFieldData_buffer);                      //This step causes a compile error (apparently scalarFieldData_buffer is the wrong data type), the error is:
+                                                                     //error: no matching function for call to ‘SCIRun::VField::set_values(std::shared_ptr<float>&)’
 */
+
+//alternate code that sets up a standard vector called 'values' that holds all the scalarFieldData_buffer data
+
+    FieldInformation lfi("LatVolMesh",1,"double");
+    MeshHandle mesh = CreateMesh(lfi,extent_sFD[0], extent_sFD[1], extent_sFD[2], Point(0.0,0.0,0.0), Point(1.0,1.0,1.0));
+    FieldHandle ofh = CreateField(lfi,mesh);
+    std::vector<float> values(numvals);
+
+    for (size_t i = 0; i < extent_sFD[0]; ++i) for (size_t j = 0; j < extent_sFD[1]; ++j) for (size_t k = 0; k < extent_sFD[2]; ++k)
+        {
+        size_t flat_index = i * extent_sFD[1] * extent_sFD[2] + j * extent_sFD[2] + k;
+        values[flat_index] = scalarFieldData_buffer.get()[flat_index];
+        }
+
+//    VMesh* omesh = ofh->vmesh();                                     //This variable is noted as not being used - possibly not needed??
+    VField* ofield = ofh->vfield();
+    ofield->set_values(values);
+
+    return ofh;
     }
 
 FieldHandle PIConGPUReader::vectorField(const int numvals, std::vector<long unsigned int> extent_vFD, std::shared_ptr<float> vFD_component_x, std::shared_ptr<float> vFD_component_y, std::shared_ptr<float> vFD_component_z)
@@ -150,7 +138,7 @@ FieldHandle PIConGPUReader::vectorField(const int numvals, std::vector<long unsi
     MeshHandle mesh = CreateMesh(lfi, extent_vFD[0], extent_vFD[1], extent_vFD[2], Point(0.0,0.0,0.0), Point(1.0,1.0,1.0));
     FieldHandle ofh = CreateField(lfi,mesh);
     
-    VMesh* omesh = ofh->vmesh();                                     //This variable is noted as not being used - possibly not needed??
+//    VMesh* omesh = ofh->vmesh();                                     //This variable is noted as not being used - possibly not needed??
     VField* ofield = ofh->vfield();
     
     for (VMesh::index_type i = 0; i < numvals; i++)
