616853851001cf46c8818ab9cf23741784c9cdc3
kimjohn1 <kimjohnpeterson@gmail.com>
2022-08-15 21:52:10 -0600

    particleData output is active

--- src/Modules/ParticleInCell/PIConGPUReader.cc
+++ src/Modules/ParticleInCell/PIConGPUReader.cc
@@ -216,10 +216,11 @@ void PIConGPUReader::execute()
 
                                                                      //Call the output function
             auto Particle_Output = particleData(buffer_size, component_x, component_y, component_z);
-/*
+
 //    *****************************************************  Set up the output data structure
             DenseMatrixHandle output_mat_0(new DenseMatrix(3,buffer_size));
             double *data0=output_mat_0->data();
+/*
             std::copy(Particle_Output, Particle_Output+buffer_size, data0);    //Need to figure out how to add an integer to a std::shared_ptr<SCIRun::Field> (all 3 functions)
 
 //    *****************************************************  Send data to the output port
@@ -254,7 +255,7 @@ void PIConGPUReader::execute()
 
 //    *****************************************************  Send data to the output port
 */
-            sendOutput(ScalarField, output_mat_1);
+//            sendOutput(ScalarField, output_mat_1);
                                                                      //End of Scalar field data processing
 
 
@@ -286,7 +287,7 @@ void PIConGPUReader::execute()
 
 //    *****************************************************  Send data to the output port
 */
-            sendOutput(VectorField, output_mat_2);
+//            sendOutput(VectorField, output_mat_2);
                                                                      //End of Vector field data processing
 
             iteration.close();
