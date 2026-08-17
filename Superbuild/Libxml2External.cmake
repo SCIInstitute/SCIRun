3975f84f9e969fcf34014646c091f55b5025dcb8
wanyong <wanyong@cs.utah.edu>
2026-08-17 16:29:58 -0600

    missing cmake files

---
+++ Superbuild/Libxml2External.cmake
@@ -0,0 +1,73 @@
+#  For more information, please see: http://software.sci.utah.edu
+#
+#  The MIT License
+#
+#  Copyright (c) 2015 Scientific Computing and Imaging Institute,
+#  University of Utah.
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+
+SET_PROPERTY(DIRECTORY PROPERTY EP_BASE ${ep_base})
+
+ExternalProject_Add(Libxml2_external
+
+  GIT_REPOSITORY ${LIBXML2_GIT_URL}
+  GIT_TAG ${LIBXML2_GIT_TAG}
+
+  DEPENDS
+    Libiconv_external
+    Zlib_external
+
+  UPDATE_COMMAND ""
+  PATCH_COMMAND ""
+
+  INSTALL_DIR ""
+
+  INSTALL_COMMAND ""
+
+  CMAKE_CACHE_ARGS
+
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=${CMAKE_VERBOSE_MAKEFILE}
+    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
+
+    -DLIBXML2_WITH_ICONV:BOOL=ON
+    -DLIBXML2_WITH_ZLIB:BOOL=ON
+
+    -DIconv_INCLUDE_DIR:PATH=${LIBICONV_INCLUDE_DIR}
+    -DIconv_LIBRARY:FILEPATH=${LIBICONV_LIBRARY}
+
+    -DZLIB_INCLUDE_DIR:PATH=${SCI_ZLIB_INCLUDE}
+    -DZLIB_LIBRARY:FILEPATH=${Zlib_DIR}
+)
+
+ExternalProject_Get_Property(Libxml2_external BINARY_DIR)
+
+SET(LibXML2_DIR ${BINARY_DIR} CACHE PATH "")
+
+MESSAGE(STATUS "LibXML2_DIR: ${LibXML2_DIR}")
+
+set(LibXML2_INCLUDE
+    ${BINARY_DIR}/include
+    CACHE PATH "libxml2 include directory")
+
+set(LibXML2_LIBRARY
+    ${BINARY_DIR}/lib/xml2.lib
+    CACHE FILEPATH "libxml2 library")
